109b888368fcb9becc433b5b611aa7d4418b1169
Sudhanshu Mishra <mrsud94@gmail.com>
2013-09-09 07:23:49 +0530

    Packaged BinPy

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='BinPy',
+    version='0.0.1',
+    author='Sudhanshu Mishra',
+    author_email='mrsud94@gmail.com',
+    url='http://pypi.python.org/pypi/BinPy/',
+    license='LICENSE.txt',
+    description='Virtualizing Electronics',
+    long_description=open('docs/README.txt').read(),
+)
