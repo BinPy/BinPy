2ade5ea33736f51b6badddc12a535145a9735221
livingstore <chahal.sarwar98@gmail.com>
2013-09-20 03:12:07 +0530

    Added IC 7412

--- BinPy/ic/series_7400.py
+++ BinPy/ic/series_7400.py
@@ -467,4 +467,37 @@ class IC_7411:
 		else:
 			print "Ground and VCC pins have not been configured correctly."
 
-#IC_7412 IC_7413 to be added
+
+    class IC_7412:
+	'''
+	This is a Triple 3 input NAND gate IC
+	'''
+	def __init__(self):
+		self.pins = [None,0,0,0,0,0,None,0,None,0,0,0,None,0,0]
+		self.gates = Gates()
+
+	def setIC(self,pin_conf):
+		'''
+		This method takes a dictionary with key:pin_no and value:pin_value
+		'''
+		for i in pin_conf:
+			self.pins[i] = pin_conf[i]
+
+	def setPin(self, pin_no, pin_value):
+		if pin_no<1 or pin_no>14:
+			sys.exit("ERROR: there are only 14 pins in this IC")
+		self.pins[pin_no] = pin_value
+
+	def run(self):
+		output = {}
+		output[12] = self.gates.NAND(self.pins[1],self.pins[2],self.pins[13])
+		output[6] = self.gates.NAND(self.pins[3],self.pins[4],self.pins[5])
+		output[8] = self.gates.NAND(self.pins[9],self.pins[10],self.pins[11])
+		if self.pins[7] == 0 and self.pins[14] == 1:
+			return output
+		else:
+			print "Ground and VCC pins have not been configured correctly."
+
+
+
+# IC_7413 to be added
