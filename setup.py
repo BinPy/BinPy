3380318446f67b8df5942589db0a9ceccd39fd6d
Sudhanshu Mishra <mrsud94@gmail.com>
2014-02-14 15:31:14 +0530

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
     name='BinPy',
-    version='0.1.7',
+    version='0.1.8',
     author='BinPy Developers',
     author_email='binpylib@gmail.com',
     url='http://pypi.python.org/pypi/BinPy/',
-    license=open('docs/LICENSE.txt').read(),
+    #license=open('docs/LICENSE.txt').read(),
     description='Virtualizing Electronics',
-    long_description=open('docs/README.txt').read(),
+    #long_description=open('docs/README.txt').read(),
 	packages=['BinPy','BinPy/binary','BinPy/combi_logic','BinPy/ic','BinPy/gates', 'BinPy/algorithms','BinPy/tools'],
-	package_data={'data': ['docs/LICENSE.txt','docs/README.txt']},
+	#package_data={'data': ['docs/LICENSE.txt','docs/README.txt']},
 	include_package_data=True,
 )
