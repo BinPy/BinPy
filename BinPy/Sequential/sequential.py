df216d3eb97ac608a5a2ccec2cc4301d2d5248e7
Anant Kandikuppa <anantk17@ymail.com>
2014-02-27 02:39:50 +0530

    Update sequential.py
    Added JK FlipFlop

--- BinPy/Sequential/sequential.py
+++ BinPy/Sequential/sequential.py
@@ -56,3 +56,35 @@ class DFlipFlop:
 		self.g1.setInput(0,input1)
 		self.g1.setInput(1,input2)
 		self.g2.setInput(1,input2)
+		
+class JKFlipFlop:
+	
+	def __init__(self,input1,input2,input3):	#input1 = J, input2 = K, input3 = enable/CLK
+		
+		self.a = Connector()
+		self.b = Connector()
+		self.c = Connector()
+		self.d = Connector()
+		
+		self.g1 = NAND(self.d,input1,input3)
+		self.g1.setOutput(self.a)
+		
+		self.g2 = NAND(input3,input2,self.b)
+		self.g2.setOutput(self.c)
+		
+		self.g3 = NAND(self.a,self.d)
+		self.g3.setOutput(self.b)
+		
+		self.g4 = NAND(self.c,self.b)
+		self.g4.setOutput(self.d)
+		
+	def output(self):
+		
+		return [self.g3.output(),self.g4.output()]
+		
+	def setInputs(self,input1,input2,input3):
+		
+		self.g1.setInput(1,input1)
+		self.g1.setInput(2,input3)
+		self.g2.setInput(0,input3)
+		self.g2.setInput(1,input2)
