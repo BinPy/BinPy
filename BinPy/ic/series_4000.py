a5ec1f7aa3191b93eabef20a5b1d1d32bee27494
gargshashank5 <garg.shashank.5@gmail.com>
2014-02-27 02:21:37 +0530

    edited doc of IC_4011, added ICs 4068, 4069, 4070

--- BinPy/ic/series_4000.py
+++ BinPy/ic/series_4000.py
@@ -75,7 +75,7 @@ class IC_4002(Base_14pin):
 
 class IC_4011(Base_14pin):
     """
-    Quad 2 input NOR gate
+    Quad 2 input NAND gate
     Pin_3 = NAND(Pin_1, Pin_2)
     Pin_4 = NAND(Pin_5, Pin_6)
     Pin_10 = NAND(Pin_8, Pin_9)
@@ -114,7 +114,6 @@ class IC_4012(Base_14pin):
         else:
             print "Ground and VCC pins have not been configured correctly."
             
-
 
 class IC_4023(Base_14pin):
     """
@@ -156,3 +155,73 @@ class IC_4025(Base_14pin):
             return output
         else:
             print "Ground and VCC pins have not been configured correctly."
+
+
+class IC_4068(Base_14pin):
+    """
+    8 input NAND gate
+    Pin_13 = NAND(Pin_2, Pin_3, Pin_4, Pin_5, Pin_9, Pin_10, Pin_11, Pin_12)
+    """
+    def __init__(self):
+        self.pins = [None,None,0,0,0,0,None,0,None,0,0,0,0,0,0]
+    
+    def run(self):
+        output = {}
+        output[13] = NAND(self.pins[2],self.pins[3],self.pins[4],
+                          self.pins[5],self.pins[9],self.pins[10],
+                          self.pins[11],self.pins[12]).output()
+        if self.pins[7] == 0 and self.pins[14] == 1:
+            return output
+        else:
+            print "Ground and VCC pins have not been configured correctly."
+
+
+class IC_4069(Base_14pin):
+    """
+    Hex NOT gate
+    Pin_2 = NOT(Pin_1)
+    Pin_4 = NOT(Pin_3)
+    Pin_6 = NOT(Pin_5)
+    Pin_8 = NOT(Pin_9)
+    Pin_10 = NOT(Pin_11)
+    Pin_12 = NOT(Pin_13)
+    """
+    def __init__(self):
+        self.pins = [None,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    
+    def run(self):
+        output = {}
+        output[2] = NOT(self.pins[1]).output()
+        output[4] = NOT(self.pins[3]).output()
+        output[6] = NOT(self.pins[5]).output()
+        output[8] = NOT(self.pins[9]).output()
+        output[10] = NOT(self.pins[11]).output()
+        output[12] = NOT(self.pins[13]).output()
+        if self.pins[7] == 0 and self.pins[14] == 1:
+            return output
+        else:
+            print "Ground and VCC pins have not been configured correctly."
+
+
+class IC_4070(Base_14pin):
+    """
+    Quad 2 input XOR gate
+    Pin_3 = XOR(Pin_1, Pin_2)
+    Pin_4 = XOR(Pin_5, Pin_6)
+    Pin_10 = XOR(Pin_8, Pin_9)
+    Pin_11 = XOR(Pin_12, Pin_13)
+    """
+    def __init__(self):
+        self.pins = [None,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    
+    def run(self):
+        output = {}
+        output[3] = XOR(self.pins[1],self.pins[2]).output()
+        output[4] = XOR(self.pins[5],self.pins[6]).output()
+        output[10] = XOR(self.pins[8],self.pins[9]).output()
+        output[11] = XOR(self.pins[12],self.pins[13]).output()
+        if self.pins[7] == 0 and self.pins[14] == 1:
+            return output
+        else:
+            print "Ground and VCC pins have not been configured correctly."
+
