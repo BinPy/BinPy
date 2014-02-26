20390721487decd5e7c22c5ec1469796472acfda
gargshashank5 <garg.shashank.5@gmail.com>
2014-02-27 01:35:13 +0530

    added 4011, 4012 ICs

--- BinPy/ic/series_4000.py
+++ BinPy/ic/series_4000.py
@@ -72,3 +72,44 @@ class IC_4002(Base_14pin):
         else:
             print "Ground and VCC pins have not been configured correctly."
 
+
+class IC_4011(Base_14pin):
+    """
+    Quad 2 input NOR gate
+    Pin_3 = NAND(Pin_1, Pin_2)
+    Pin_4 = NAND(Pin_5, Pin_6)
+    Pin_10 = NAND(Pin_8, Pin_9)
+    Pin_11 = NAND(Pin_12, Pin_13)
+    """
+    def __init__(self):
+        self.pins = [None,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    
+    def run(self):
+        output = {}
+        output[3] = NAND(self.pins[1],self.pins[2]).output()
+        output[4] = NAND(self.pins[5],self.pins[6]).output()
+        output[10] = NAND(self.pins[8],self.pins[9]).output()
+        output[11] = NAND(self.pins[12],self.pins[13]).output()
+        if self.pins[7] == 0 and self.pins[14] == 1:
+            return output
+        else:
+            print "Ground and VCC pins have not been configured correctly."
+
+class IC_4012(Base_14pin):
+    """
+    Dual 4 input NAND gate
+    Pin_1 = NAND(Pin_2, Pin_3, Pin_4, Pin_5)
+    Pin_13 = NAND(Pin_9, Pin_10, Pin_11, Pin_12)    
+    """
+    def __init__(self):
+        self.pins = [None,0,0,0,0,0,None,0,None,0,0,0,0,0,0]
+    
+    def run(self):
+        output = {}
+        output[1] = NAND(self.pins[2],self.pins[3],self.pins[4],self.pins[5]).output()
+        output[13] = NAND(self.pins[9],self.pins[10],self.pins[11],self.pins[12]).output()
+        if self.pins[7] == 0 and self.pins[14] == 1:
+            return output
+        else:
+            print "Ground and VCC pins have not been configured correctly."
+
