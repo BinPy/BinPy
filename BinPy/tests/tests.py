2c4efc68ad6751a13140b38d5c9e0d4164b04e8c
gargshashank5 <garg.shashank.5@gmail.com>
2014-02-27 20:11:14 +0530

    added tests for ICs 4000, 4001, 4002, 4011, 4012, 4023, 4025, 4068, 4069, 4070

--- BinPy/tests/tests.py
+++ BinPy/tests/tests.py
@@ -370,6 +370,88 @@ def test_IC_74260():
     if q!=testIC.run():
         assert False
 
+######## 4000 series IC's with 7 pins ####################
+
+def test_IC_4000():
+    testIC = IC_4000()
+    p = {3:1,4:1,5:1,7:0,8:1,11:0,12:0,13:0,14:1}
+    testIC.setIC(p)
+    q = {6:0,9:0,10:1}
+    if q!=testIC.run():
+        assert False
+        
+def test_IC_4001():
+    testIC = IC_4001()
+    p = {1:0,2:0,5:0,6:1,7:0,8:1,9:0,12:1,13:1,14:1}
+    testIC.setIC(p)
+    q = {3:1,4:0,10:0,11:0}
+    if q!=testIC.run():
+        assert False
+    
+def test_IC_4002():
+    testIC = IC_4002()
+    p = {2:0,3:0,4:0,5:0,7:0,9:1,10:1,11:1,12:1,14:1}
+    testIC.setIC(p)
+    q = {1:1,13:0}
+    if q!=testIC.run():
+        assert False
+    
+def test_IC_4011():
+    testIC = IC_4011()
+    p = {1:0,2:0,5:0,6:1,7:0,8:1,9:0,12:1,13:1,14:1}
+    testIC.setIC(p)
+    q = {3:1,4:1,10:1,11:0}
+    if q!=testIC.run():
+        assert False
+    
+def test_IC_4012():
+    testIC = IC_4012()
+    p = {2:0,3:1,4:0,5:1,7:0,9:1,10:1,11:1,12:1,14:1}
+    testIC.setIC(p)
+    q = {1:1, 13:0}
+    if q!=testIC.run():
+        assert False
+        
+def test_IC_4023():
+    testIC = IC_4023()
+    p = {1:1,2:1,3:0,4:0,5:0,7:0,8:1,11:0,12:1,13:1,14:1}
+    testIC.setIC(p)
+    q = {6:1,9:0,10:1}
+    if q!=testIC.run():
+        assert False
+        
+def test_IC_4025():
+    testIC = IC_4025()
+    p = {1:1,2:1,3:0,4:0,5:0,7:0,8:1,11:0,12:1,13:1,14:1}
+    testIC.setIC(p)
+    q = {6:1,9:0,10:0}
+    if q!=testIC.run():
+        assert False
+        
+def test_IC_4068():
+    testIC = IC_4068()
+    p = {2:1,3:1,4:0,5:1,7:0,9:1,10:0,11:1,12:1,14:1}
+    testIC.setIC(p)
+    q = {13:1}
+    if q!=testIC.run():
+        assert False
+        
+def test_IC_4069():
+    testIC = IC_4069()
+    p = {1:0,3:1,5:1,7:0,9:0,11:0,13:0,14:1}
+    testIC.setIC(p)
+    q = {2:1,4:0,6:0,8:1,10:1,12:1}
+    if q!=testIC.run():
+        assert False
+        
+def test_IC_4070():
+    testIC = IC_4070()
+    p = {1:0,2:0,5:0,6:1,7:0,8:1,9:0,12:1,13:1,14:1}
+    testIC.setIC(p)
+    q = {3:0,4:1,10:1,11:0}
+    if q!=testIC.run():
+        assert False
+        
 ######## IC's with 5 pins #################################
 
 def test_IC_741G00():
