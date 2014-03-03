7bbaa379b675dbe922553ca8f2dc8b8d765f827b
gargshashank5 <garg.shashank.5@gmail.com>
2014-03-03 23:46:03 +0530

    adds example files for ICs 4000, 4001, 4002, 4011, 4012, 4023, 4025, 4068, 4069, 4070, 4071, 4072, 4073, 4075, 4077, 4078, 4081 and 4082

---
+++ BinPy/examples/ic/Series_4000/IC4070.py
@@ -0,0 +1,19 @@
+from BinPy import *
+print 'Usage of IC 4070:\n'
+ic = IC_4070()
+print '\nThe Pin configuration is:\n'
+input = {1: 0, 2: 0, 5: 0, 6: 1, 7: 0, 8: 1, 9: 0, 12: 1, 13: 1, 14: 1}
+print input
+print '\nPin initinalization\n'
+print ic.setIC(input)
+print '\nPowering up the IC - using -- ic.setIC({14: 1, 7: 0}) -- \n'
+ic.setIC({14: 1, 7: 0})
+print '\nDraw the IC with the current configuration\n'
+ic.drawIC()
+print '\nRun the IC with the current configuration using -- print ic.run() -- \n'
+print 'Note that the ic.run() returns a dict of pin configuration similar to :'
+print ic.run()
+print '\nSeting the outputs to the current IC configuration using -- ic.setIC(ic.run()) --\n'
+ic.setIC(ic.run())
+print '\nDraw the final configuration\n'
+ic.drawIC()
