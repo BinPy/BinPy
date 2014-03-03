b51a4204c1e3efd15259baee0ace95d2f2a3a557
SSR09 <sandeepreddys09@gmail.com>
2014-03-03 12:17:00 +0530

    Clean Up

--- BinPy/Gates/connector.py
+++ BinPy/Gates/connector.py
@@ -1,23 +1,17 @@
 class Connector:
-        ''' Connector is analogous to wire (in VHDL)
-        Can have three states: 1 (True), 0 (False), None (High Impedance State)
-        Can get an output from more than one logic objects. A warning is printed when there is a logic contention
-        Can provide input to more than one logic objects.'''
 
-	state = None # To store the state of the connector
+	state = 0 # To store the state of the connection
 	
+
 	def __init__(self):
-		self.input_to = list() # To store all the objects to which this connector is an input
-		self.output_of = list() # To store all the objects to which this connector is an output
+		self.connections = list() # To store the all the taps onto this connection
 
 	def tap(self,element,mode):
-                if (mode == 'input'):
-                        self.input_to.append(element)
-                else if (mode == 'output'):
-                        self.output_of.append(element)
+		self.connections.append([element,mode]) # Add an element to the connections list
 		
 	#This fucntion is called when the value of the connection changes
 	def trigger(self):
 
-		for i in self.input_to:
-                        i.trigger()
+		for i in self.connections:
+			if i[1] == 'input':
+				i[0].trigger()
--- BinPy/Gates/gates.py
+++ BinPy/Gates/gates.py
@@ -14,64 +14,51 @@ class GATES:
 		self.outputConnector = None #Valid only if outputType = 1
 		self.inputs = inputs[:] # Set the inputs
 		self.history_inputs = [] # Save a copy of the inputs
-		self._updateConnections(self.inputs)
+		self.__updateConnections(self.inputs)
 		self._updateHistory()
 
 		self.trigger() # Any change in the input will trigger change in the output
 	
-	def _updateConnections(self,inputs):
+	def __updateConnections(self,inputs):
 
 		for i in inputs:
 			if isinstance(i,Connector):
 				i.tap(self,'input')
 
-        def setInput(self, index, value):
-                '''Sets the value of an input at the specified index if the input.
 
-                index - Starting from zero, the index of the input whose value is to be set.
-                value - The value to be assigned (can be 0,1 or None)
-                Exception is raised if an input which is not present is tried to be set.
-                Exception is raised if the output of a logic object is tried to be set.'''
+	def setInputs(self,*inputs):
 
-                if index >= len(self.inputs) or index < 0:
-                        raise Exception("ERROR: Index value greater than number of inputs to the gate")
-                if len(self.inputs[index].output_of) != 0:
-                        raise Exception("ERROR: Tried to assign a value to a connector class")
-                if isinstance(self.input[index], Connector):
-                        self.inputs[index].state = inputs_list[index]
-                        self.inputs[index].trigger()
-                else:
-                        self.inputs[index] = inputs_list[index]
-                        self.trigger()
+		#Clean Connections before updating new connections
+		if len(inputs) < 2:
+			raise Exception("ERROR: Too few inputs given")
+			return None
 
+		else:
+			self.history_active = 1 #Use history before computing
+			self.inputs = list(inputs)[:] # Set the inputs
+			self.__updateConnections(self.inputs)
 
-	def setInputs(self, inputs_list):
-                ''' Takes a LIST of input values and assigns it to the corresponding inputs of a Gate.
-                An exception is raised if the lengths of the list does not agree with the number of inputs to the Gate.
-                The value of 0, 1 or None can be assigned to an input connection if it is not an output of any other logic object.
-                If it is an output of any logic object, one HAS to provide the corresponding element in the list as '~'
-                Else an exception will be raised.
-                '~' can also be used if one does not want to change value of an input.
-                Eg. of valid list input: ['~',1,0,'~',0,'~'] if 0th, 3rd and 5th inputs cannot/should not be changed.'''
+		self.trigger() # Any change in the input will trigger change in the output
 
-                if len(inputs_list) != len(self.inputs):
-                        raise Exception("ERROR: Given number of inputs does not match with the number of inputs of the gate")
+	def setInput(self,index,value):
 
-                self.history_active = 1 #Use history before computing
-                for index in range(len(inputs_list)):
-                        if inputs_list[index] == '~':
-                                pass
-                        else:
-##                              self.history_inputs[index] = self.inputs[index]
-                                if len(self.inputs[index].output_of) != 0:
-                                        raise Exception("ERROR: Tried to assign a value to a connector class")
-                                if isinstance(self.input[index], Connector):
-                                        self.inputs[index].state = inputs_list[index]
-                                        self.inputs[index].trigger()
-                                else:
-                                        self.inputs[index] = inputs_list[index]
-                                        self.trigger()
-                        
+		if index > len(self.inputs):
+			self.inputs.append(value) #If the index is more than the length then append to the list
+			self.history_active = 0 # Dont use history after a new input is added
+			self._updateHistory() # because history_active is set to 0 trigger will get called irrespective of the history.
+
+		else:
+			self.history_active = 1 # Use history before computing
+			if isinstance(self.inputs[index],Connector):
+				self.history_inputs[index]  = self.inputs[index].state
+			else:
+				self.history_inputs[index] = self.inputs[index] # Modify the history
+			self.inputs[index] = value
+
+		if isinstance(value,Connector):
+			value.tap(self,'input')
+
+		self.trigger()
 
 	def getInputStates(self):
                 input_states = []
@@ -82,13 +69,11 @@ class GATES:
                                 input_states.append(self.inputs[i])
 		return input_states
 
-	def _updateResult(self):
+	def _updateResult(self,value):
 		
-		#self.result = int(value) 
+		self.result = int(value) #Set True or False
 		if self.outputType == 1:
-                        if outputConnector.state != None #if the connector was not having a value of None
-                                print('WARNING: Logic Contention detected') #then the new output would mean a contention
-			self.outputConnector.state = self.result #the connector would however be loaded with this result of this gate
+			self.outputConnector.state = self.result
 
 	def _updateHistory(self):
 
@@ -148,15 +133,13 @@ class AND(GATES):
 
 		if self._compareHistory() == True:
 			self.history_active = 1
-			self.result = True
+			self._updateResult(True)
 			self._updateHistory() # Update the inputs after a computation
 
 			for i in self.inputs:
 				if (isinstance(i,Connector) and i.state == False) or i == False:
-					self.result = False
+					self._updateResult(False)
 					break
-
-			self._updateResult()
 
 			if self.outputType:
 				self.outputConnector.trigger()
@@ -176,15 +159,13 @@ class OR(GATES):
 
 		if self._compareHistory() == True:
 			self.history_active = 1
-			self.result = False
+			self._updateResult(False)
 			self._updateHistory() # Update the inputs after a computation
 			
 			for i in self.inputs:
 				if (isinstance(i,Connector) and i.state == True) or i == True:
-					self.result = True
+					self._updateResult(True)
 					break
-
-			self._updateResult()
 
 			if self.outputType:
 				self.outputConnector.trigger()
@@ -194,7 +175,7 @@ class NOT(GATES):
 	def __init__(self,*inputs):
 
 		if len(inputs) != 1:
-			raise Exception("ERROR: NOT Gates take only one input")
+			raise Exception("ERROR: NOT Gates takes only one input")
 			return None
 
 		else:
@@ -206,38 +187,11 @@ class NOT(GATES):
 			self.history_active = 1
 			#self._updateResult(True)
 			self._updateHistory() # Update the inputs after a computation
-			self.result = not self.inputs[0]
-			self._updateResult()
+			
+			self._updateResult( not self.inputs[0] )
 
 			if self.outputType:
 				self.outputConnector.trigger()
-
-class BUFFER(GATES):
-        ''' Assumes that the first argument is an input to the gate and
-        the second one is the enable input of the buffer.'''
-
-	def __init__(self,*inputs):
-
-		if len(inputs) != 2:
-			raise Exception("ERROR: BUFFER Gates take only two inputs - Input and Enable")
-			return None
-
-		else:
-			GATES.__init__(self,list(inputs))
-
-	def trigger(self):
-
-		if self._compareHistory() == True:
-			self.history_active = 1
-			self.result = None
-			self._updateHistory() # Update the inputs after a computation
-			if inputs[1] == True
-                                self.result = inputs[0]
-			self._updateResult()
-
-			if self.outputType:
-				self.outputConnector.trigger()
-
 
 class XOR(GATES):
 
@@ -254,7 +208,7 @@ class XOR(GATES):
 
 		if self._compareHistory() == True:
 			self.history_active = 1
-			#self._updateResult(True)
+			self._updateResult(True)
 			self._updateHistory() # Update the inputs after a computation
 			
 			temp = self.inputs[0]
@@ -262,8 +216,7 @@ class XOR(GATES):
 			for i in self.inputs[1:]:
 				temp = temp ^ i
 
-                        self.result = temp
-			self._updateResult()
+			self._updateResult(temp)
 
 			if self.outputType:
 				self.outputConnector.trigger()
@@ -283,7 +236,7 @@ class XNOR(GATES):
 
 		if self._compareHistory() == True:
 			self.history_active = 1
-			#self._updateResult(True)
+			self._updateResult(True)
 			self._updateHistory() # Update the inputs after a computation
 			
 			temp = self.inputs[0]
@@ -291,8 +244,7 @@ class XNOR(GATES):
 			for i in self.inputs[1:]:
 				temp = temp ^ i
 
-                        self.result = (not temp)
-			self._updateResult()
+			self._updateResult( not temp)
 
 			if self.outputType:
 				self.outputConnector.trigger()
@@ -312,7 +264,7 @@ class NAND(GATES):
 
 		if self._compareHistory() == True:
 			self.history_active = 1
-			self.result = False
+			self._updateResult(False)
 			self._updateHistory() # Update the inputs after a computation
 			
 			#temp = self.inputs[0]
@@ -321,10 +273,9 @@ class NAND(GATES):
 
 			for i in self.inputs:
 				if (isinstance(i,Connector) and i.state == False) or i == False:
-					self.result=True
+					self._updateResult(True)
 					break
-				
-                        self._updateResult()
+
 			if self.outputType:
 				self.outputConnector.trigger()
 
@@ -343,16 +294,15 @@ class NOR(GATES):
 
 		if self._compareHistory() == True:
 			self.history_active = 1
-			self.result = True
+			self._updateResult(True)
 			self._updateHistory() # Update the inputs after a computation
 			
 			#self._updateResult(True)
 
 			for i in self.inputs:
 				if (isinstance(i,Connector) and i.state == True) or i == True:
-					self.result = False
+					self._updateResult(False)
 					break
-                        self._updateResult()
 
 			if self.outputType:
 				self.outputConnector.trigger()
