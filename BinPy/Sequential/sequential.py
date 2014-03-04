1f9fd1e6f253d35a2018363fb2c9eeea6798833f
Aliya Hameer <adhameer@gmail.com>
2014-03-04 17:56:43 -0500

    Updated sequential.py to conform to PEP8 guidelines.

--- BinPy/Sequential/sequential.py
+++ BinPy/Sequential/sequential.py
@@ -1,64 +1,66 @@
 from BinPy.Gates import *
 
+
 class SRLatch:
 
-	def __init__(self, R, S):
-		"""
-		Construct an SRLatch with initial reset input R and set input S.
-		"""
+    def __init__(self, R, S):
+        """
+        Construct an SRLatch with initial reset input R and set input S.
+        """
 
-		self.a = Connector()
-		self.b = Connector()
+        self.a = Connector()
+        self.b = Connector()
 
-		self.g1 = NOR(R, self.b)
-		self.g1.setOutput(self.a)
+        self.g1 = NOR(R, self.b)
+        self.g1.setOutput(self.a)
 
-		self.g2 = NOR(S, self.a)
-		self.g2.setOutput(self.b)
+        self.g2 = NOR(S, self.a)
+        self.g2.setOutput(self.b)
 
-	def setInputs(self, R, S):
-		"""
-		Set this SRLatch's reset input to R and set input to S.
-		Unstable behaviour when transitioning directly from RS = 11 to RS = 00.
-		"""
+    def setInputs(self, R, S):
+        """
+        Set this SRLatch's reset input to R and set input to S.
+        Unstable behaviour when transitioning directly from RS = 11 to RS = 00.
+        """
 
-		self.g1.setInputs(R, self.b)
-		self.g2.setInputs(S, self.a)
+        self.g1.setInput(0, R)
+        self.g2.setInput(0, S)
 
-	def output(self):
-		"""
-		Return the output of this SRLatch in the format [Q, ~Q].
-		"""
+    def output(self):
+        """
+        Return the output of this SRLatch in the format [Q, Q'].
+        """
 
-		return [self.g1.output(), self.g2.output()]
+        return [self.g1.output(), self.g2.output()]
+
 
 class DFlipFlop:
 
-	def __init__(self,input1,input2):
+    def __init__(self, input1, input2):
 
-		self.a = Connector()
-		self.b = Connector()
-		self.c = Connector()
-		self.d = Connector()
+        self.a = Connector()
+        self.b = Connector()
+        self.c = Connector()
+        self.d = Connector()
 
-		self.g1 = NAND(input1,input2)
-		self.g1.setOutput(self.a)
+        self.g1 = NAND(input1, input2)
+        self.g1.setOutput(self.a)
 
-		self.g2 = NAND(self.a,input2)
-		self.g2.setOutput(self.b)
+        self.g2 = NAND(self.a, input2)
+        self.g2.setOutput(self.b)
 
-		self.g3 = NAND(self.a,self.d)
-		self.g3.setOutput(self.c)
+        self.g3 = NAND(self.a, self.d)
+        self.g3.setOutput(self.c)
 
-		self.g4 = NAND(self.b,self.c)
-		self.g4.setOutput(self.d)
+        self.g4 = NAND(self.b, self.c)
+        self.g4.setOutput(self.d)
 
-	def output(self):
+    def output(self):
 
-		return [self.g3.output(),self.g4.output()]
+        return [self.g3.output(), self.g4.output()]
 
-	def setInputs(self,input1,input2):
+    def setInputs(self, input1, input2):
 
-		self.g1.setInput(0,input1)
-		self.g1.setInput(1,input2)
-		self.g2.setInput(1,input2)
+        self.g1.setInput(0, input1)
+        self.g1.setInput(1, input2)
+        self.g2.setInput(1, input2)
