f9c71845b062aa031de1eafeab7c11116b5540b4
gargshashank5 <garg.shashank.5@gmail.com>
2014-03-06 01:13:18 +0530

    adds example files for 4000 series ICs

---
+++ BinPy/examples/ic/Series_4000/IC4012.py
@@ -0,0 +1,18 @@
+from BinPy import *
+print 'Usage of IC 4012:\n'
+ic = IC_4012()
+print '\nThe Pin configuration is:\n'
+input = {2: 0, 3: 1, 4: 0, 5: 1, 7: 0, 9: 1, 10: 1, 11: 1, 12: 1, 14: 1}
+print input
+print '\nPin initinalization\n'
+print '\nPowering up the IC - using -- ic.setIC({14: 1, 7: 0}) -- \n'
+ic.setIC({14: 1, 7: 0})
+print '\nDraw the IC with the current configuration\n'
+ic.drawIC()
+print '\nRun the IC with the current configuration using -- print ic.run() -- \n'
+print 'Note that the ic.run() returns a dict of pin configuration similar to :'
+print ic.run()
+print '\nSeting the outputs to the current IC configuration using -- ic.setIC(ic.run()) --\n'
+ic.setIC(ic.run())
+print '\nDraw the final configuration\n'
+ic.drawIC()
