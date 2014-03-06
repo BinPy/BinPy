f42cf3039e5e98d3bfef26ef42a1d02f54f6b0a1
SSR09 <sandeepreddys09@gmail.com>
2014-03-06 23:48:53 +0530

    Implemented conversion of any Boolean Expression to only NAND or NOR implementation Also has two input gate constraint Also has functionality of implementing only using AND, OR and NOT gates

--- BinPy/examples/Algorithms/ExpressionConvertExample.py
+++ BinPy/examples/Algorithms/ExpressionConvertExample.py
@@ -9,6 +9,21 @@ print("Obtained Expression:\n"+converted)
 expr = '((A AND B)xor(NOT(B) and C) xor(C and NOT(D)))or   E or NOT(F)'
 print("Given Expression:\n"+expr)
 converted = convertExpression(expr)
-converted2 = convertExpression(expr,1)
+converted2 = convertExpression(expr,two_input=1)
 print("Obtained Expression:\n"+converted)
 print("Obtained Expression with two input gate constraint:\n"+converted2)
+
+expr = '(A XOR B XOR C)'
+print("\nGiven Expression:\n"+expr)
+converted = convertExpression(expr)
+print("Obtained Expression:\n"+converted)
+converted = convertExpression(expr, two_input = 1)
+print("Obtained Expression with 2 input constraint:\n"+converted)
+converted = convertExpression(expr, only_and_or_not = 1)
+print("Equivalent Expression with only AND, OR & NOT gates is:\n"+converted)
+expr = 'A XOR B'
+print("\nGiven Expression:\n"+expr)
+converted = convertExpression(expr, only_nand = 1)
+print("Equivalent Expression with only NAND gates is:\n"+converted)
+converted = convertExpression(expr, only_nor = 1)
+print("Equivalent Expression with only NAND gates is:\n"+converted)
