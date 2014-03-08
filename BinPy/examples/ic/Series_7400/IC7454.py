b14bf3a97bceb62b4387afb6fd35068dc4774af0
Raghav R V <rvraghav93@gmail.com>
2014-03-08 20:43:29 +0530

    Made examples for IC Series 7400 PEP8 compliant

--- BinPy/examples/ic/Series_7400/IC7454.py
+++ BinPy/examples/ic/Series_7400/IC7454.py
@@ -1,20 +1,23 @@
 from __future__ import print_function
 from BinPy import *
-print ( 'Usage of IC 7454:\n')
+print ('Usage of IC 7454:\n')
 ic = IC_7454()
-print ( '\nThe Pin configuration is:\n')
+print ('\nThe Pin configuration is:\n')
 p = {1: 1, 2: 0, 3: 0, 4: 0, 7: 0, 9: 1, 10: 1, 11: 0, 12: 0, 13: 0, 14: 1}
-print ( p)
-print ( '\nPin initialization -using -- ic.setIC(p) --\n')
+print (p)
+print ('\nPin initialization -using -- ic.setIC(p) --\n')
 ic.setIC(p)
-print ( '\nPowering up the IC - using -- ic.setIC({14:1,7:0}) -- \n')
-ic.setIC({14:1,7:0})
-print ( '\nDraw the IC with the current configuration\n')
+print ('\nPowering up the IC - using -- ic.setIC({14:1,7:0}) -- \n')
+ic.setIC({14: 1, 7: 0})
+print ('\nDraw the IC with the current configuration\n')
 ic.drawIC()
-print ( '\nRun the IC with the current configuration using -- print ic.run() -- \n')
-print ( 'Note that the ic.run() returns a dict of pin configuration similar to :')
-print ( ic.run())
-print ( '\nSeting the outputs to the current IC configuration using -- ic.setIC(ic.run()) --\n')
+print (
+    '\nRun the IC with the current configuration using -- print ic.run() -- \n')
+print (
+    'Note that the ic.run() returns a dict of pin configuration similar to :')
+print (ic.run())
+print (
+    '\nSeting the outputs to the current IC configuration using -- ic.setIC(ic.run()) --\n')
 ic.setIC(ic.run())
-print ( '\nDraw the final configuration\n')
+print ('\nDraw the final configuration\n')
 ic.drawIC()
