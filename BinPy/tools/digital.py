8f5e370c6ba407d3ca3496ee74c1fc9fb6851693
Raghav R V <rvraghav93@gmail.com>
2014-03-08 17:20:50 +0530

    Corrected the clock to reflect the current implementation of Connector class

--- BinPy/tools/digital.py
+++ BinPy/tools/digital.py
@@ -43,7 +43,7 @@ class Clock(threading.Thread):
         self.curr_state = init_state
         self.exitFlag = 0
         self.daemon = True
-        self.A = Connector(self, 'A', activates=1, monitor=0)
+        self.A = Connector(0)
 
     def __toggleState(self):
         """
@@ -51,15 +51,15 @@ class Clock(threading.Thread):
         """
         if self.curr_state==1:
             self.curr_state = 0
-            self.A.set(self.curr_state)
+            self.A.state = self.curr_state
         else:
             self.curr_state = 1
-            self.A.set(self.curr_state)
+            self.A.state = self.curr_state
 
     def __main_func(self):
         while True:
             if self.exitFlag:
-                thread.exit()
+                sys.exit()
             time.sleep(self.time_period)
             try:
                 self.__toggleState()
