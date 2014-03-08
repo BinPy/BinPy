7c71478769e8355ed61aa13f70b726d1a7318350
Raghav R V <rvraghav93@gmail.com>
2014-03-08 17:51:03 +0530

    Added examples for 2 Counters

---
+++ BinPy/examples/Counters/NBitRippleCounter.py
@@ -0,0 +1,28 @@
+#Example for N Bit Binary Ripple Counter.
+
+from BinPy import *
+
+toggle = Connector(1)
+
+#Initialize the clock
+clock = Clock(1,100)
+clock.start()
+#A clock of 100 hertz frequency
+clk_conn = clock.A
+
+enable = Connector(1)
+
+print 'Setting No of Bits to 8'
+print 'Clock frequency is 100 Hz'
+
+#Initializing the counter
+b = NBitRippleCounter(8,clk_conn)
+
+print 'INITIAL STATE'
+print b.state()
+
+print 'TRIGGERING THE COUNTER SEQUENTIALLY 2^8 + 1 times'
+
+for i in range(1,2**8 + 1):
+    b.trigger()
+    print b.state()
