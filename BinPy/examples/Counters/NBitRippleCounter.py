765b60fa608713f43776495aecb81df34845702a
Raghav R V <rvraghav93@gmail.com>
2014-03-08 20:55:31 +0530

    Files from rebase

--- BinPy/examples/Counters/NBitRippleCounter.py
+++ BinPy/examples/Counters/NBitRippleCounter.py
@@ -19,13 +19,13 @@ print ("Setting No of Bits to 8")
 print ("Clock frequency is 100 Hz")
 
 # Initializing the counter
-b = NBitRippleCounter(8,clk_conn)
+b = NBitRippleCounter(8, clk_conn)
 
 print ("INITIAL STATE")
 print (b.state())
 
 print ("TRIGGERING THE COUNTER SEQUENTIALLY 2^8 + 1 times")
 
-for i in range(1,2**8 + 1):
+for i in range(1, 2 ** 8 + 1):
     b.trigger()
     print (b.state())
