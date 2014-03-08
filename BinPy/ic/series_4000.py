3fed3192c00f8cd24a243bc332e83304def9f0ac
Raghav R V <rvraghav93@gmail.com>
2014-03-08 20:52:41 +0530

    Making all the files in ic PEP8 compliant

--- BinPy/ic/series_4000.py
+++ BinPy/ic/series_4000.py
@@ -18,12 +18,14 @@ from BinPy.ic.base import *
 
 
 class IC_4000(Base_14pin):
+
     """
     Dual 3 Input NOR gate + one NOT gate IC.
     Pin_6 = NOR(Pin_3, Pin_4, Pin_5)
     Pin_10 = NOR(Pin_11, Pin_12, Pin_13)
     Pin_9 = NOT(Pin_8)
     """
+
     def __init__(self):
         self.pins = [None, None, None, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
         self.pins = pinlist_quick(self.pins)
@@ -60,6 +62,7 @@ class IC_4000(Base_14pin):
 
 
 class IC_4001(Base_14pin):
+
     """
     Quad 2 input NOR gate
     Pin_3 = NOR(Pin_1, Pin_2)
@@ -67,6 +70,7 @@ class IC_4001(Base_14pin):
     Pin_10 = NOR(Pin_8, Pin_9)
     Pin_11 = NOR(Pin_12, Pin_13)
     """
+
     def __init__(self):
         self.pins = [None, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.pins = pinlist_quick(self.pins)
@@ -102,11 +106,13 @@ class IC_4001(Base_14pin):
 
 
 class IC_4002(Base_14pin):
+
     """
     Dual 4 input NOR gate
     Pin_1 = NOR(Pin_2, Pin_3, Pin_4, Pin_5)
     Pin_13 = NOR(Pin_9, Pin_10, Pin_11, Pin_12)
     """
+
     def __init__(self):
         self.pins = [None, 0, 0, 0, 0, 0, None, 0, None, 0, 0, 0, 0, 0, 0]
         self.pins = pinlist_quick(self.pins)
@@ -142,6 +148,7 @@ class IC_4002(Base_14pin):
 
 
 class IC_4011(Base_14pin):
+
     """
     Quad 2 input NAND gate
     Pin_3 = NAND(Pin_1, Pin_2)
@@ -149,6 +156,7 @@ class IC_4011(Base_14pin):
     Pin_10 = NAND(Pin_8, Pin_9)
     Pin_11 = NAND(Pin_12, Pin_13)
     """
+
     def __init__(self):
         self.pins = [None, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.pins = pinlist_quick(self.pins)
@@ -183,11 +191,13 @@ class IC_4011(Base_14pin):
 
 
 class IC_4012(Base_14pin):
+
     """
     Dual 4 input NAND gate
     Pin_1 = NAND(Pin_2, Pin_3, Pin_4, Pin_5)
     Pin_13 = NAND(Pin_9, Pin_10, Pin_11, Pin_12)
     """
+
     def __init__(self):
         self.pins = [None, 0, 0, 0, 0, 0, None, 0, None, 0, 0, 0, 0, 0, 0]
         self.pins = pinlist_quick(self.pins)
@@ -222,12 +232,14 @@ class IC_4012(Base_14pin):
 
 
 class IC_4023(Base_14pin):
+
     """
     Triple 3 input NAND gate
     Pin_6 = NAND(Pin_3, Pin_4, Pin_5)
     Pin_9 = NAND(Pin_1, Pin_2, Pin_8)
     Pin_10 = NAND(Pin_11, Pin_12, Pin_13)
     """
+
     def __init__(self):
         self.pins = [None, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.pins = pinlist_quick(self.pins)
@@ -264,12 +276,14 @@ class IC_4023(Base_14pin):
 
 
 class IC_4025(Base_14pin):
+
     """
     Triple 3 input NOR gate
     Pin_6 = NOR(Pin_3, Pin_4, Pin_5)
     Pin_9 = NOR(Pin_1, Pin_2, Pin_8)
     Pin_10 = NOR(Pin_11, Pin_12, Pin_13)
     """
+
     def __init__(self):
         self.pins = [None, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.pins = pinlist_quick(self.pins)
@@ -306,10 +320,12 @@ class IC_4025(Base_14pin):
 
 
 class IC_4068(Base_14pin):
+
     """
     8 input NAND gate
     Pin_13 = NAND(Pin_2, Pin_3, Pin_4, Pin_5, Pin_9, Pin_10, Pin_11, Pin_12)
     """
+
     def __init__(self):
         self.pins = [None, None, 0, 0, 0, 0, None, 0, None, 0, 0, 0, 0, 0, 0]
         self.pins = pinlist_quick(self.pins)
@@ -344,6 +360,7 @@ class IC_4068(Base_14pin):
 
 
 class IC_4069(Base_14pin):
+
     """
     Hex NOT gate
     Pin_2 = NOT(Pin_1)
@@ -353,6 +370,7 @@ class IC_4069(Base_14pin):
     Pin_10 = NOT(Pin_11)
     Pin_12 = NOT(Pin_13)
     """
+
     def __init__(self):
         self.pins = [None, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.pins = pinlist_quick(self.pins)
@@ -389,6 +407,7 @@ class IC_4069(Base_14pin):
 
 
 class IC_4070(Base_14pin):
+
     """
     Quad 2 input XOR gate
     Pin_3 = XOR(Pin_1, Pin_2)
@@ -396,6 +415,7 @@ class IC_4070(Base_14pin):
     Pin_10 = XOR(Pin_8, Pin_9)
     Pin_11 = XOR(Pin_12, Pin_13)
     """
+
     def __init__(self):
         self.pins = [None, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.pins = pinlist_quick(self.pins)
@@ -430,6 +450,7 @@ class IC_4070(Base_14pin):
 
 
 class IC_4071(Base_14pin):
+
     """
     Quad 2 input OR gate
     Pin_3 = OR(Pin_1, Pin_2)
@@ -437,6 +458,7 @@ class IC_4071(Base_14pin):
     Pin_10 = OR(Pin_8, Pin_9)
     Pin_11 = OR(Pin_12, Pin_13)
     """
+
     def __init__(self):
         self.pins = [None, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.pins = pinlist_quick(self.pins)
@@ -471,11 +493,13 @@ class IC_4071(Base_14pin):
 
 
 class IC_4072(Base_14pin):
+
     """
     Dual 4 input OR gate
     Pin_1 = OR(Pin_2, Pin_3, Pin_4, Pin_5)
     Pin_13 = OR(Pin_9, Pin_10, Pin_11, Pin_12)
     """
+
     def __init__(self):
         self.pins = [None, 0, 0, 0, 0, 0, None, 0, None, 0, 0, 0, 0, 0, 0]
         self.pins = pinlist_quick(self.pins)
@@ -510,12 +534,14 @@ class IC_4072(Base_14pin):
 
 
 class IC_4073(Base_14pin):
+
     """
     Triple 3 input AND gate
     Pin_6 = AND(Pin_3, Pin_4, Pin_5)
     Pin_9 = AND(Pin_1, Pin_2, Pin_8)
     Pin_10 = AND(Pin_11, Pin_12, Pin_13)
     """
+
     def __init__(self):
         self.pins = [None, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.pins = pinlist_quick(self.pins)
@@ -552,12 +578,14 @@ class IC_4073(Base_14pin):
 
 
 class IC_4075(Base_14pin):
+
     """
     Triple 3 input OR gate
     Pin_6 = OR(Pin_3, Pin_4, Pin_5)
     Pin_9 = OR(Pin_1, Pin_2, Pin_8)
     Pin_10 = OR(Pin_11, Pin_12, Pin_13)
     """
+
     def __init__(self):
         self.pins = [None, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.pins = pinlist_quick(self.pins)
@@ -594,6 +622,7 @@ class IC_4075(Base_14pin):
 
 
 class IC_4077(Base_14pin):
+
     """
     Quad 2 input XNOR gate
     Pin_3 = XNOR(Pin_1, Pin_2)
@@ -601,6 +630,7 @@ class IC_4077(Base_14pin):
     Pin_10 = XNOR(Pin_8, Pin_9)
     Pin_11 = XNOR(Pin_12, Pin_13)
     """
+
     def __init__(self):
         self.pins = [None, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.pins = pinlist_quick(self.pins)
@@ -635,10 +665,12 @@ class IC_4077(Base_14pin):
 
 
 class IC_4078(Base_14pin):
+
     """
     8 input NOR gate
     Pin_13 = NOR(Pin_2, Pin_3, Pin_4, Pin_5, Pin_9, Pin_10, Pin_11, Pin_12)
     """
+
     def __init__(self):
         self.pins = [None, None, 0, 0, 0, 0, None, 0, None, 0, 0, 0, 0, 0, 0]
         self.pins = pinlist_quick(self.pins)
@@ -673,6 +705,7 @@ class IC_4078(Base_14pin):
 
 
 class IC_4081(Base_14pin):
+
     """
     Quad 2 input AND gate
     Pin_3 = AND(Pin_1, Pin_2)
@@ -680,6 +713,7 @@ class IC_4081(Base_14pin):
     Pin_10 = AND(Pin_8, Pin_9)
     Pin_11 = AND(Pin_12, Pin_13)
     """
+
     def __init__(self):
         self.pins = [None, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.pins = pinlist_quick(self.pins)
@@ -714,11 +748,13 @@ class IC_4081(Base_14pin):
 
 
 class IC_4082(Base_14pin):
+
     """
     Dual 4 input AND gate
     Pin_1 = AND(Pin_2, Pin_3, Pin_4, Pin_5)
     Pin_13 = AND(Pin_9, Pin_10, Pin_11, Pin_12)
     """
+
     def __init__(self):
         self.pins = [None, 0, 0, 0, 0, 0, None, 0, None, 0, 0, 0, 0, 0, 0]
         self.pins = pinlist_quick(self.pins)
