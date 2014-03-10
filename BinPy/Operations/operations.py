23b10f312a0006a6097d28590b8c57dd2642fe79
Raghav R V <rvraghav93@gmail.com>
2014-03-10 19:02:28 +0530

    PEP8 - Operations folder

--- BinPy/Operations/operations.py
+++ BinPy/Operations/operations.py
@@ -1,24 +1,25 @@
 class Operations:
+
     """
     This class implements the primary arithmetic binary functions ADD, SUB, MUL, DIV, COMP(complement).
     Inputs are in the form of unsigned integers. Negative numbers will have - sign.
     """
 
-    def __parseInput(self,input1,input2):
+    def __parseInput(self, input1, input2):
 
-        if isinstance(input1,list):
-            input1 = ''.join(map(str,input1))
-        elif not isinstance(input1,str):
+        if isinstance(input1, list):
+            input1 = ''.join(map(str, input1))
+        elif not isinstance(input1, str):
             input1 = str(input1)
 
-        if isinstance(input2,list):
-            input2 = ''.join(map(str,input2))
-        elif not isinstance(input2,str):
+        if isinstance(input2, list):
+            input2 = ''.join(map(str, input2))
+        elif not isinstance(input2, str):
             input2 = str(input2)
 
-        return input1,input2
+        return input1, input2
 
-    def ADD(self,input1,input2):
+    def ADD(self, input1, input2):
         """
         This function implements the binary addition
         It takes two binary number and gives their sum
@@ -28,14 +29,14 @@ class Operations:
             '1101'
         """
 
-        a,b = self.__parseInput(input1,input2)
-        c = bin(int(a,2) + int(b,2))
+        a, b = self.__parseInput(input1, input2)
+        c = bin(int(a, 2) + int(b, 2))
         if c[0] == '-':
             return c[3:]
         else:
             return c[2:]
 
-    def SUB(self,input1,input2):
+    def SUB(self, input1, input2):
         """
         This function implements the binary subtraction
         It takes two binary number and gives their difference
@@ -45,14 +46,14 @@ class Operations:
             '1000'
         """
 
-        a,b = self.__parseInput(input1,input2)
-        c = bin(int(a,2) - int(b,2))
+        a, b = self.__parseInput(input1, input2)
+        c = bin(int(a, 2) - int(b, 2))
         if c[0] == '-':
             return c[3:]
         else:
             return c[2:]
 
-    def MUL(self,input1,input2):
+    def MUL(self, input1, input2):
         """
         This function implements the binary multiplication
         It takes two binary number and gives their product
@@ -62,14 +63,14 @@ class Operations:
             '110000'
         """
 
-        a,b = self.__parseInput(input1,input2)
-        c = bin(int(a,2) * int(b,2))
+        a, b = self.__parseInput(input1, input2)
+        c = bin(int(a, 2) * int(b, 2))
         if c[0] == '-':
             return c[3:]
         else:
             return c[2:]
 
-    def DIV(self,input1,input2):
+    def DIV(self, input1, input2):
         """
         This function implements the binary division
         It takes two binary number and gives their quotient
@@ -79,14 +80,14 @@ class Operations:
             '100'
         """
 
-        a,b = self.__parseInput(input1,input2)
-        c = bin(int(a,2) // int(b,2))
+        a, b = self.__parseInput(input1, input2)
+        c = bin(int(a, 2) // int(b, 2))
         if c[0] == '-':
             return c[3:]
         else:
             return c[2:]
 
-    def COMP(self,input1,option):
+    def COMP(self, input1, option):
         """
         This function gives the complement of the input
         Note: In this case the input is put in the form of signed/unsigned number
@@ -97,18 +98,17 @@ class Operations:
             '0111'
         """
 
-        if isinstance(input1,list):
-            input1 = ''.join(map(str,input1))
-        elif not isinstance(input1,str):
+        if isinstance(input1, list):
+            input1 = ''.join(map(str, input1))
+        elif not isinstance(input1, str):
             input1 = str(input1)
 
-        result = bin(int(input1,2) ^ int(len(input1)*'1',2))[2:]
-        temp = bin(int(result,2) + int('1',2))[2:]
+        result = bin(int(input1, 2) ^ int(len(input1) * '1', 2))[2:]
+        temp = bin(int(result, 2) + int('1', 2))[2:]
         if str(option) == '1':
             return (len(input1) - len(result)) * '0' + result
         else:
             return (len(input1) - len(temp)) * temp[0] + temp
-
 
     @staticmethod
     def decToBin(number):
@@ -121,8 +121,6 @@ class Operations:
 
         return int(bin(number)[2:])
 
-  
-  
     @staticmethod
     def binToDec(number):
         """
@@ -131,5 +129,5 @@ class Operations:
             >>> Operations.binToDec('1001')
             >>> 9
         """
-  
-        return int(number,2) 
+
+        return int(number, 2)
