dfb1292929dc68d281a9076fa417f58587728bae
JBC <jbcsound@gmail.com>
2014-03-11 13:44:02 -0300

    Reimplemented tri-state Buffer as a Gate subclass.

--- BinPy/Tristate/tristate.py
+++ BinPy/Tristate/tristate.py
@@ -1,20 +1,19 @@
-from BinPy.Gates.connector import *
+from BinPy.Gates import *
 
-class buffer(object):
-    def __init__(self, a, b, c):
-        is_connector(a, b, c)
-        self.a, self.b, self.c = a, b, c
+class Buffer(Gate):
+    def __init__(self, c, b, a):
+        Gate.__init__(self, c, b, a)
 
-    def trigger(self):
-        if self.b.state == 0:
-            self.c.set(2)
-        elif self.b.state == 1:
-            self.c.set(self.a.state)
+    def calc_output(self, in_states):
+        if self.inputs[0] == 0:
+            return 2
+        elif self.inputs[0] == 1:
+            return self.inputs[1]
         else:
-            self.c.set(3)
+            return 0
 
-
-class bus(object):
+# Useful to implement output enable in ICs
+class Bus(object):
     pass
 
 
