7a5fcfe6c43f7b9d3651ec140e4e2e11f2c1e6c5
mohitgujarathi14 <mohitontherocks14@gmail.com>
2014-03-16 09:36:02 +0530

    Update __init__.py

--- BinPy/Algorithms/__init__.py
+++ BinPy/Algorithms/__init__.py
@@ -1,3 +1,5 @@
 from BinPy.Algorithms.QuineMcCluskey import QM
 from BinPy.Algorithms.MooreOptimizer import *
 from BinPy.Algorithms.AnalogFormulas import *
+from BinPy.Algorithms.ExpressionConvert import *
+from BinPy.Algorithms.ImplementBooleanFunction import *
