61831c01896e4f9be4e0db48cbf72207b9ae6640
Raghav R V <rvraghav93@gmail.com>
2014-03-22 18:25:06 +0530

    Added oscilloscope in init

--- BinPy/tools/__init__.py
+++ BinPy/tools/__init__.py
@@ -2,3 +2,4 @@ from BinPy.tools.digital import *
 from BinPy.tools.powersource import *
 from BinPy.tools.ground import *
 from BinPy.tools.multivibrator import *
+from BinPy.tools.oscilloscope import *
