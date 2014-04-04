358ccbd497cdbd8e7a6b3d7db937722c3ac56306
petermlm <pedromelgueira@gmail.com>
2014-04-05 00:22:08 +0100

    Made better comments for new alterations to the Tree class

--- BinPy/Gates/tree.py
+++ BinPy/Gates/tree.py
@@ -17,10 +17,19 @@ from BinPy.Gates.connector import *
 class Tree:
 
     '''
-    This class returns a tree representation of a digital element, such as a
-    gate, and its inputs. The second argument specifies the depth to which the
-    representation shows digital elements instead of inputs. To illustrate a
-    use case, suppose the following code:
+    This class is a tree representation of a digital element, such as a
+    gate, and its inputs. The class uses the backtrack() function which follows
+    the element and tracks the inputs, and inputs of inputs, and so on, thus
+    constructing the tree.
+
+    The tree construction has the possibility to not follow cycles so the final
+    output is simpler.
+
+    The backtrack() function is called in the constructor of the class.
+
+    The printTree() function can be used to print the Tree in a redable way.
+    The following examples show two use cases, one of which shows what happens
+    if cycles are not being followed.
 
     Examples
     ========
@@ -30,28 +39,77 @@ class Tree:
     >>> g3 = AND(g1, g2)
     >>> tree = Tree(g3, 2)
     >>> tree.backtrack()
-    (g3, [(g1, [True, False]), (g2, [True, False])])
+    >>> tree.printTree()
+    |- AND Gate; Output: 0; Inputs: [0, 0];
+       |- AND Gate; Output: 0; Inputs: [True, False];
+          |- True
+          |- False
+       |- AND Gate; Output: 0; Inputs: [True, False];
+          |- True
+          |- False
 
-    This tree representation is made using tuples, where the first element of
-    tuple is the node (gate, connector, mux, etc.), and the second is a list
-    of inputs. In that list each element will also be a tuple with the same
-    representation. In some cases instead of a tuple there will be a boolean
-    or int value, meaning the maximum depth as been reached.
+    If the algorithm was executed to not follow cycles, the output will have
+    marks indicating repetitions. In the following example the elements
+    marked with [0] are the same and have no sons to avoid repetitive
+    output. The same for the elements with [1].
+
+    >>> c1 = Connector(True)
+    >>> c2 = Connector(True)
+    >>> g1 = AND(True, c1)
+    >>> g2 = AND(c2, False)
+    >>> g3 = AND(g1, g2)
+    >>> g4 = AND(g3, True)
+    >>> g3.setOutput(c1)
+    >>> g4.setOutput(c2)
+    |- [1] AND Gate; Output: 0; Inputs: [0, True];
+       |- [0] AND Gate; Output: 0; Inputs: [0, 0];
+          |- AND Gate; Output: 0; Inputs: [True, 0];
+             |- True
+             |- Connector; State: 0
+                |- [0] AND Gate; Output: 0; Inputs: [0, 0];
+          |- AND Gate; Output: 0; Inputs: [0, False];
+             |- Connector; State: 0
+                |- [1] AND Gate; Output: 0; Inputs: [0, True];
+             |- False
+       |- True
     '''
 
-    def __init__(self, element, depth, cycles=True):
+    def __init__(self, element, depth=1, cycles=True):
+        '''
+        Constructor for the tree class
+
+        Keyword arguments:
+        element -- Any digital element, such as a gate. this gate will be
+                   the root of the tree. The inputs will be the sons.
+        depth   -- Depth until which the inputs are tracked. (default 1)
+        cycles  -- If the tree such track cycles in the circuits or not. (default True)
+        '''
         self.element = element
         self.depth = depth
         self.cycles = cycles
 
         self.sons = []
 
+        # Make an initial backtrack
+        self.backtrack()
+
     def setDepth(self, val):
+        '''
+        Sets depth until which the tree is constructed.
+
+        val -- New depth.
+        '''
+
         self.depth = val
 
     def backtrack(self, hist=None):
         '''
-        Returns the backtrack hierarchy of the tree up to self.depth
+        Constructs the backtrack hierarchy of the tree up to self.depth.
+
+        Keyword arguments:
+        hist -- An instance of CycleHist. A class which maintains the passed
+                tracked if backtrack is not following cycles. Should only be
+                used internally.
         '''
 
         # Store new history if available, or create new one
@@ -105,22 +163,9 @@ class Tree:
         The way a gate, or a mux or any other digital element gets
         represented depends on it's __str__() implementation.
 
-        Examples
-        ========
-
-        >>> g1 = AND(True, False)
-        >>> g2 = AND(True, False)
-        >>> g3 = AND(g1, g2)
-        >>> tree = Tree(g3, 2)
-        >>> tree.backtrack()
-        >>> tree.printTree()
-        |- AND Gate; Output: 0; Inputs: [0, 0];
-           |- AND Gate; Output: 0; Inputs: [True, False];
-              |- True
-              |- False
-           |- AND Gate; Output: 0; Inputs: [True, False];
-              |- True
-              |- False
+        Keyword arguments:
+        space -- Number of spaces which are going to be printed in each
+                 recursive step. Should only be used internally. (default 0)
         '''
         self.printTuple(self.node)
 
@@ -151,21 +196,24 @@ class Tree:
 
 
 class CycleHist:
-    """
+    '''
     This class helps to keep the cycle history of a circuit by registering
     occurrences of a digital element. The class has a dictionary that stores
     an instance of CycleHistValue for each key element.
-    """
+    '''
 
     def __init__(self):
         self.hist = {}
         self.current_index = 0
 
     def regOccurrence(self, element):
-        """
+        '''
         Register an occurrence for an element. If the element has been seen
         before, mark that element has a repeating element.
-        """
+
+        Keyword arguments:
+        element -- Any digital element to be added to the dictionary.
+        '''
 
         # If the element has been seen before
         if element in self.hist.keys():
@@ -183,38 +231,63 @@ class CycleHist:
             self.hist[element] = CycleHistValue()
 
     def getIndex(self, element):
-        """
+        '''
         Get the repetition index for the given element
-        """
+
+        Keyword arguments:
+        element -- A digital element in the dictionary
+        '''
 
         return self.hist[element].getIndex()
 
     def isRepeated(self, element):
-        """
-        heck if the given element is repeting or not
-        """
+        '''
+        Check if the given element is repeating or not
+
+        Keyword arguments:
+        element -- The element that is being check if it is repeated or not.
+        '''
 
         return self.hist[element].isRepeated()
 
 
 class CycleHistValue:
-    """
-    This class represents the value in the dictionary of the cycleHist class.
+    '''
+    This class represents the value in the dictionary of the CycleHist class.
     It has the index of the element and if it has been repeated or not.
-    """
+    '''
 
     def __init__(self):
         self.repeated = False
         self.index = 0
 
     def setIndex(self, index):
+        '''
+        Set the index of the element for which this instance is associated.
+
+        Keyword arguments:
+        index -- The index in question.
+        '''
         self.index = index
 
     def getIndex(self):
+        '''
+        Get index of the element of this instance.
+        '''
+
         return self.index
 
     def setRepeated(self):
+        '''
+        Set is the element of this instance is repeated or not.
+        '''
+
         self.repeated = True
 
     def isRepeated(self):
+        '''
+        Check if the element for which this instance is associated is repeated
+        or not.
+        '''
+
         return self.repeated
