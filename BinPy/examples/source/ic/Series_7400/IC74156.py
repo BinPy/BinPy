e238900c286550426f9a9cb724272f091f9a0edf
Raghav R V <rvraghav93@gmail.com>
2014-04-24 08:19:45 +0530

    Examples: Replaced all the examples with the cleaner ones

---
+++ BinPy/examples/source/ic/Series_7400/IC74156.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <headingcell level=2>
+
+# Usage of IC 74156
+
+# <codecell>
+
+from __future__ import print_function
+from BinPy import *
+
+# <codecell>
+
+# Usage of IC 74156:
+
+ic = IC_74156()
+
+print(ic.__doc__)
+
+# <codecell>
+
+# The Pin configuration is:
+
+inp = {1: 1, 2: 0, 3: 0, 13: 1, 8: 0, 16: 1, 15: 1, 14: 0}
+
+# Pin initinalization
+
+# Powering up the IC - using -- ic.setIC({14: 1, 7: 0})
+
+ic.setIC({14: 1, 7: 0})
+
+# Setting the inputs of the ic
+
+ic.setIC(inp)
+
+# Draw the IC with the current configuration\n
+
+ic.drawIC()
+
+# <codecell>
+
+# Run the IC with the current configuration using -- print ic.run() -- 
+
+# Note that the ic.run() returns a dict of pin configuration similar to 
+
+print (ic.run())
+
+# <codecell>
+
+# Seting the outputs to the current IC configuration using -- ic.setIC(ic.run()) --\n
+
+ic.setIC(ic.run())
+
+# Draw the final configuration
+
+ic.drawIC()
+
+# <codecell>
+
+# Seting the outputs to the current IC configuration using -- ic.setIC(ic.run()) --
+
+ic.setIC(ic.run())
+
+# Draw the final configuration
+
+ic.drawIC()
+
+# Run the IC
+
+print (ic.run())
+
+# <codecell>
+
+# Connector Outputs
+c = Connector()
+
+# Set the output connector to a particular pin of the ic
+ic.setOutput(6, c)
+
+print(c)
+
---
+++ BinPy/examples/source/ic/Series_7400/IC7451.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <headingcell level=2>
+
+# Usage of IC 7451
+
+# <codecell>
+
+from __future__ import print_function
+from BinPy import *
+
+# <codecell>
+
+# Usage of IC 7451:
+
+ic = IC_7451()
+
+print(ic.__doc__)
+
+# <codecell>
+
+# The Pin configuration is:
+
+inp = {
+    1: 1,
+    2: 1,
+    3: 0,
+    4: 0,
+    5: 0,
+    7: 0,
+    9: 0,
+    10: 0,
+    11: 0,
+    12: 1,
+    13: 1,
+    14: 1}
+
+# Pin initinalization
+
+# Powering up the IC - using -- ic.setIC({14: 1, 7: 0})
+
+ic.setIC({14: 1, 7: 0})
+
+# Setting the inputs of the ic
+
+ic.setIC(inp)
+
+# Draw the IC with the current configuration\n
+
+ic.drawIC()
+
+# <codecell>
+
+# Run the IC with the current configuration using -- print ic.run() -- 
+
+# Note that the ic.run() returns a dict of pin configuration similar to 
+
+print (ic.run())
+
+# <codecell>
+
+# Seting the outputs to the current IC configuration using -- ic.setIC(ic.run()) --\n
+
+ic.setIC(ic.run())
+
+# Draw the final configuration
+
+ic.drawIC()
+
+# <codecell>
+
+# Seting the outputs to the current IC configuration using -- ic.setIC(ic.run()) --
+
+ic.setIC(ic.run())
+
+# Draw the final configuration
+
+ic.drawIC()
+
+# Run the IC
+
+print (ic.run())
+
+# <codecell>
+
+# Connector Outputs
+c = Connector()
+
+# Set the output connector to a particular pin of the ic
+ic.setOutput(8, c)
+
+print(c)
+
