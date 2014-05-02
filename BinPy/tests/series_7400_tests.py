9c9b511e104faa52f52957fa8590ef608faa5160
Raghav R V <rvraghav93@gmail.com>
2014-05-03 03:36:09 +0530

    PEP8 and removed debug line

--- BinPy/tests/series_7400_tests.py
+++ BinPy/tests/series_7400_tests.py
@@ -371,11 +371,24 @@ def test_IC_7458():
     if q != testIC.run():
         assert False
 
+
 def test_IC_7459():
     testIC = IC_7459()
-    p = {14:1, 7:0, 2:1, 3:0, 4:0, 5:1, 1: 1, 13: 1, 12: 1, 11: 1, 10: 1, 9: 1}
+    p = {
+        14: 1,
+        7: 0,
+        2: 1,
+        3: 0,
+        4: 0,
+        5: 1,
+        1: 1,
+        13: 1,
+        12: 1,
+        11: 1,
+        10: 1,
+        9: 1}
     testIC.setIC(p)
-    q = {6:1, 8: 0}
+    q = {6: 1, 8: 0}
     if q != testIC.run():
         assert False
 
@@ -544,7 +557,6 @@ def test_IC_7445():
     p = {15: 0, 14: 1, 13: 0, 12: 0, 8: 0, 16: 1}
     testIC.setIC(p)
     q = {1: 1, 2: 1, 3: 0, 4: 1, 5: 1, 6: 1, 7: 1, 9: 1, 10: 1, 11: 1}
-    print testIC.run()
     if q != testIC.run():
         assert False
 
