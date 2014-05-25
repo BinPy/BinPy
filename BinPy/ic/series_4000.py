53fde42b76b44c98234532bb10a9580b81e001a3
Raghav R V <rvraghav93@gmail.com>
2014-05-26 02:34:38 +0530

    ic: IC4008 reimplemented as 4 Bit Carry Look Ahead Adder fixes #210

--- BinPy/ic/series_4000.py
+++ BinPy/ic/series_4000.py
@@ -969,61 +969,61 @@ class IC_4008(Base_16pin):
                     })
 
 
-def run(self):
-    output = {}
-    output[10] = ((self.pins[2]()) ^ (self.pins[3]()) ^ (self.pins[9]()))()
-    output[11] = (
-        (self.pins[5]()) ^ (
-            self.pins[4]()) ^ (
-            (self.pins[7]() & self.pins[6]()) | (
-                (self.pins[7]() | self.pins[6]()) & (
-                    self.pins[9]()))))()
-    output[12] = (
-        (self.pins[3]()) ^ (
-            self.pins[2]()) ^ (
-            (self.pins[5]() & self.pins[4]()) | (
-                (self.pins[5]() | self.pins[4]()) & (
-                    self.pins[7]() & self.pins[6]())) | (
-                        (self.pins[5]() | self.pins[4]()) & (
-                            self.pins[7]() | self.pins[6]()) & (
-                                self.pins[9]()))))()
-    output[13] = (
-        (self.pins[1]()) ^ (
-            self.pins[15]()) ^ (
-            (self.pins[3]() & self.pins[2]()) | (
-                (self.pins[3]() | self.pins[2]()) & (
+    def run(self):
+        output = {}
+        output[10] = ((self.pins[2]()) ^ (self.pins[3]()) ^ (self.pins[9]()))()
+        output[11] = (
+            (self.pins[5]()) ^ (
+                self.pins[4]()) ^ (
+                (self.pins[7]() & self.pins[6]()) | (
+                    (self.pins[7]() | self.pins[6]()) & (
+                        self.pins[9]()))))()
+        output[12] = (
+            (self.pins[3]()) ^ (
+                self.pins[2]()) ^ (
+                (self.pins[5]() & self.pins[4]()) | (
+                    (self.pins[5]() | self.pins[4]()) & (
+                        self.pins[7]() & self.pins[6]())) | (
+                            (self.pins[5]() | self.pins[4]()) & (
+                                self.pins[7]() | self.pins[6]()) & (
+                                    self.pins[9]()))))()
+        output[13] = (
+            (self.pins[1]()) ^ (
+                self.pins[15]()) ^ (
+                (self.pins[3]() & self.pins[2]()) | (
+                    (self.pins[3]() | self.pins[2]()) & (
+                        self.pins[5]() & self.pins[4]())) | (
+                            (self.pins[3]() | self.pins[2]()) & (
+                                self.pins[5]() | self.pins[4]()) & (
+                                    self.pins[7]() & self.pins[6]())) | (
+                                        (self.pins[3]() | self.pins[2]()) & (
+                                            self.pins[5]() | self.pins[4]()) & (
+                                                self.pins[7]() | self.pins[6]()) & (
+                                                    self.pins[9]()))))()
+        output[14] = (
+            (self.pins[1]() & self.pins[15]()) | (
+                (self.pins[1]() | self.pins[15]()) & (
+                    self.pins[3]() & self.pins[2]())) | (
+                (self.pins[1]() | self.pins[15]()) & (
+                    self.pins[3]() | self.pins[2]()) & (
                     self.pins[5]() & self.pins[4]())) | (
-                        (self.pins[3]() | self.pins[2]()) & (
-                            self.pins[5]() | self.pins[4]()) & (
-                                self.pins[7]() & self.pins[6]())) | (
-                                    (self.pins[3]() | self.pins[2]()) & (
-                                        self.pins[5]() | self.pins[4]()) & (
-                                            self.pins[7]() | self.pins[6]()) & (
-                                                self.pins[9]()))))()
-    output[14] = (
-        (self.pins[1]() & self.pins[15]()) | (
-            (self.pins[1]() | self.pins[15]()) & (
-                self.pins[3]() & self.pins[2]())) | (
-            (self.pins[1]() | self.pins[15]()) & (
-                self.pins[3]() | self.pins[2]()) & (
-                self.pins[5]() & self.pins[4]())) | (
-            (self.pins[1]() | self.pins[15]()) & (
-                self.pins[3]() | self.pins[2]()) & (
-                self.pins[5]() | self.pins[4]()) & (
-                self.pins[7]() & self.pins[6]())) | (
-            (self.pins[1]() | self.pins[15]()) & (
-                self.pins[3]() | self.pins[2]()) & (
-                self.pins[5]() | self.pins[4]()) & (
-                self.pins[7]() | self.pins[6]()) & (
-                self.pins[9]())))()
+                (self.pins[1]() | self.pins[15]()) & (
+                    self.pins[3]() | self.pins[2]()) & (
+                    self.pins[5]() | self.pins[4]()) & (
+                    self.pins[7]() & self.pins[6]())) | (
+                (self.pins[1]() | self.pins[15]()) & (
+                    self.pins[3]() | self.pins[2]()) & (
+                    self.pins[5]() | self.pins[4]()) & (
+                    self.pins[7]() | self.pins[6]()) & (
+                    self.pins[9]())))()
 
-    if self.pins[8].value == 0 and self.pins[16].value == 1:
-        self.setIC(output)
-        for i in self.outputConnector:
-            self.outputConnector[i].state = output[i]
-        return output
-    else:
-        print ("Ground and VCC pins have not been configured correctly.")
+        if self.pins[8].value == 0 and self.pins[16].value == 1:
+            self.setIC(output)
+            for i in self.outputConnector:
+                self.outputConnector[i].state = output[i]
+            return output
+        else:
+            print ("Ground and VCC pins have not been configured correctly.")
 
 
 class IC_4009(Base_16pin):
