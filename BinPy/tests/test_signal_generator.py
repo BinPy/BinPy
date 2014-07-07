2944598acbb29344ad6c9d780abf315616ddb8d9
Raghav R V <rvraghav93@gmail.com>
2014-07-08 01:56:57 +0530

    sig_gen tests: Sampling done only at thread pauses; Testing accuracy down to 10mv; Increased freq to reduce testing time.

--- BinPy/tests/test_signal_generator.py
+++ BinPy/tests/test_signal_generator.py
@@ -4,7 +4,7 @@ from BinPy import *
 
 
 def test_sin_signal():
-    sig1 = SignalGenerator(typ=0, freq=10, ampl=1)
+    sig1 = SignalGenerator(typ=0, freq=1000, ampl=1)
     time.sleep(0.5)  # To allow setup time.
 
     max = -1
@@ -12,24 +12,28 @@ def test_sin_signal():
 
     start_time = time.time()
     while (time.time() - start_time) < sig1.time_period:
+        # sample when the run method is in sleep
+        while sig1.updating:
+            pass
+
         t = sig1.last_updated_time
+        sig1_output = sig1.outputs[0].voltage - sig1.outputs[1].voltage
+        sin_t = 0.5 * math.sin(2 * 3.145926 * sig1._frequency * t) + 0.5
 
-        sig1_output = float(sig1.outputs[0]) - float(sig1.outputs[1])
-        sig1_output = int(sig1_output * 1000) / float(1000)
-
-        sin_t = 0.5 * math.sin(2 * math.pi * sig1._frequency * t) + 0.5
-        sin_t = int(sin_t * 1000) / float(1000)
-
-        assert (sig1_output == sin_t)
+        if (round(sig1_output, 2) != round(sin_t, 2)):
+            sig1.kill()
+            assert False
 
         time.sleep(sig1.sampling_time_interval)
-        
+
     sig1.kill()
 
+
 def test_square_signal():
-    sig1 = SignalGenerator(typ=1, freq=10, ampl=2)
+    sig1 = SignalGenerator(typ=1, freq=1000, ampl=2)
     sig1.set_offset(-1)
-    time.sleep(0.5)  # To allow setup time.
+    time.sleep(0.5)
+    # To allow setup time.
     # To make range [ -1 to 1 ]
 
     max = -1
@@ -37,23 +41,25 @@ def test_square_signal():
 
     start_time = time.time()
     while (time.time() - start_time) < sig1.time_period:
+        # sample when the run method is in sleep
+        while sig1.updating:
+            pass
+
         t = sig1.last_updated_time
-
-        sig1_output = float(sig1.outputs[0]) - float(sig1.outputs[1])
-        sig1_output = int(sig1_output * 1000) / float(1000)
-
+        sig1_output = sig1.outputs[0].voltage - sig1.outputs[1].voltage
         sq_t = 1 if t < (sig1.time_period / float(2)) else -1
-        sq_t = int(sq_t * 1000) / float(1000)
 
-        assert (sig1_output == sq_t)
+        if (round(sig1_output, 2) != round(sq_t, 2)):
+            sig1.kill()
+            assert False
 
         time.sleep(sig1.sampling_time_interval)
-        
+
     sig1.kill()
 
 
 def test_ramp_signal():
-    sig1 = SignalGenerator(typ=2, freq=10, ampl=2)
+    sig1 = SignalGenerator(typ=2, freq=1000, ampl=2)
     sig1.set_offset(-1)
     time.sleep(0.5)  # To allow setup time.
     # To make range [ -1 to 1 ]
@@ -63,23 +69,25 @@ def test_ramp_signal():
 
     start_time = time.time()
     while (time.time() - start_time) < sig1.time_period:
+        # sample when the run method is in sleep
+        while sig1.updating:
+            pass
+
         t = sig1.last_updated_time
-
-        sig1_output = float(sig1.outputs[0]) - float(sig1.outputs[1])
-        sig1_output = int(sig1_output * 1000) / float(1000)
-
+        sig1_output = sig1.outputs[0].voltage - sig1.outputs[1].voltage
         r_t = 2 * ((t / sig1.time_period) - 0.5)
-        r_t = int(r_t * 1000) / float(1000)
 
-        assert (sig1_output == r_t)
+        if (round(sig1_output, 2) != round(r_t, 2)):
+            sig1.kill()
+            assert False
 
         time.sleep(sig1.sampling_time_interval)
-        
+
     sig1.kill()
 
 
 def test_triangle_signal():
-    sig1 = SignalGenerator(typ=3, freq=10, ampl=1)
+    sig1 = SignalGenerator(typ=3, freq=1000, ampl=1)
     time.sleep(0.5)  # To allow setup time.
 
     max = -1
@@ -87,34 +95,29 @@ def test_triangle_signal():
 
     start_time = time.time()
     while (time.time() - start_time) < sig1.time_period:
+        # sample when the run method is in sleep
+        while sig1.updating:
+            pass
+
         t = sig1.last_updated_time
+        sig1_output = sig1.outputs[0].voltage - sig1.outputs[1].voltage
 
-        sig1_output = float(sig1.outputs[0]) - float(sig1.outputs[1])
-        sig1_output = int(sig1_output * 1000) / float(1000)
+        if (t < (sig1.time_period / 2.0)):
+            tr_t = (2 * t / sig1.time_period)
+        else:
+            tr_t = (2 * (sig1.time_period - t) / sig1.time_period)
 
-        tr_t = (
-            2 *
-            t /
-            sig1.time_period) if (
-            t < (
-                sig1.time_period /
-                float(2))) else (
-                2 *
-                (
-                    sig1.time_period -
-                    t) /
-            sig1.time_period)
-        tr_t = int(tr_t * 1000) / float(1000)
-
-        assert (sig1_output == tr_t)
+        if (round(sig1_output, 2) != round(tr_t, 2)):
+            sig1.kill()
+            assert False
 
         time.sleep(sig1.sampling_time_interval)
-        
+
     sig1.kill()
 
 
 def test_ttl_signal():
-    sig1 = SignalGenerator(typ=4, freq=10, ampl=2)
+    sig1 = SignalGenerator(typ=4, freq=1000, ampl=2)
     time.sleep(0.5)  # To allow setup time.
 
     max = -1
@@ -122,16 +125,18 @@ def test_ttl_signal():
 
     start_time = time.time()
     while (time.time() - start_time) < sig1.time_period:
+        # sample when the run method is in sleep
+        while sig1.updating:
+            pass
+
         t = sig1.last_updated_time
+        sig1_output = sig1.outputs[0].voltage - sig1.outputs[1].voltage
+        ttl_t = 5 if t < (sig1.time_period / 2.0) else 0
 
-        sig1_output = float(sig1.outputs[0]) - float(sig1.outputs[1])
-        sig1_output = int(sig1_output * 1000) / float(1000)
-
-        ttl_t = 5 if t < (sig1.time_period / float(2)) else 0
-        ttl_t = int(ttl_t * 1000) / float(1000)
-
-        assert (sig1_output == ttl_t)
+        if (round(sig1_output, 2) != round(ttl_t, 2)):
+            sig1.kill()
+            assert False
 
         time.sleep(sig1.sampling_time_interval)
-        
+
     sig1.kill()
