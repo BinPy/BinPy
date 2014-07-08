6eb291ea4d15eae14fb72fca190116a4644fe709
Raghav R V <rvraghav93@gmail.com>
2014-07-08 06:25:18 +0530

    Added AM Synthesis in docs

--- BinPy/analog/sig_gen.py
+++ BinPy/analog/sig_gen.py
@@ -143,6 +143,42 @@ class SignalGenerator(threading.Thread):
     >>> print ( table )
     (0, 0.2), ()
 
+    AM SYNTHESIS USING 2 SIGNAL GENERATOR INSTANCES
+    ===============================================
+
+    from BinPy import SignalGenerator
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import math, time
+
+    m_t = SignalGenerator(typ = 0, freq = 10, ampl = 1)
+    m_t.set_offset(-0.5)
+
+    c_t = SignalGenerator(typ = 0, freq = 100, ampl = 10)
+    c_t.set_offset(-5) # To make the range as [-5, 5]
+
+    c_t.set_modulation_input(m_t.outputs)
+    c_t.set_modulation_type(1)
+
+    time.sleep(0.5) # To allow setup time
+
+    c_t.last_updated_time, (c_t.outputs[0].voltage - c_t.outputs[1].voltage)
+
+    data = np.zeros(shape = (2, math.ceil(m_t.time_period / c_t.sampling_time_interval)))
+
+    for i in range(data.shape[1]):
+        data[0][i] = m_t.last_updated_time + m_t.time_period * i
+        data[1][i] = c_t.outputs[0].voltage - c_t.outputs[1].voltage
+        time.sleep(c_t.sampling_time_interval)
+
+    fig, ax = plt.subplots()
+
+    ax.plot(data[0], data[1])
+
+    plt.show()
+
+    m_t.kill()
+    c_t.kill()
 
     """
 
