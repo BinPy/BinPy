322f4b3a0e9fdc7898464c5f6ed47431311e5a36
Raghav R V <rvraghav93@gmail.com>
2014-07-17 03:20:00 +0530

    Added bittools and MultiplicationAlgos in __init__

--- BinPy/algorithms/__init__.py
+++ BinPy/algorithms/__init__.py
@@ -3,3 +3,4 @@ from BinPy.algorithms.QuineMcCluskey import QM
 from BinPy.algorithms.MooreOptimizer import *
 from BinPy.algorithms.AnalogFormulas import *
 from BinPy.algorithms.ExpressionConvert import *
+from BinPy.algorithms.bittools import *
