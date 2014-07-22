42e57a2c6d0100353ef5c7757056c62c5fa8d2ee
Sudhanshu Mishra <mrsud94@gmail.com>
2014-07-22 17:13:29 +0530

    Misc fixes to sig_gen.py

--- BinPy/analog/sig_gen.py
+++ BinPy/analog/sig_gen.py
@@ -11,53 +11,59 @@ class SignalGenerator(threading.Thread):
     Signal Generator Block
     ======================
 
-    Create a SignalGenerator object ( runs on a child thread ) used to generate an analog voltage signal of the desired type.
-    The frequency, amplitude and modulation parameters can be customized.
+    Create a SignalGenerator object ( runs on a child thread ) used to generate
+    an analog voltage signal of the desired type. The frequency and amplitude
+    parameters can be customized.
 
     All inputs / outputs / enable are Bus types
 
     METHODS
     =======
 
-    set_enable(enable_Bus_1_bit)     : link the enable_Bus_1_bit with the Buffer's enable.
+    set_enable(enable_Bus_1_bit) : link the enable_Bus_1_bit with the Buffer's enable.
 
-    set_type(integer_type)           : Set the output waveform type.
+    set_type(integer_type)       : Set the output waveform type.
 
-                                       ================================================================
-                                       | TYPE_NO   |    TYPE         |    Built-in Constant Name      |
-                                       ================================================================
-                                       |   0       |    Sine         |    SignalGenerator.SIN         |
-                                       |   1       |    Square       |    SignalGenerator.SQUARE      |
-                                       |   2       |    Ramp         |    SignalGenerator.RAMP        |
-                                       |   3       |    Triangular   |    SignalGenerator.TRIANGULAR  |
-                                       |   4       |    TTL          |    SignalGenerator.TTL         |
-                                       ================================================================
+                                   ==============================================================
+                                   | TYPE_NO   |    TYPE       |    Built-in Constant Name      |
+                                   ==============================================================
+                                   |   0       |    Sine       |    SignalGenerator.SIN         |
+                                   |   1       |    Square     |    SignalGenerator.SQUARE      |
+                                   |   2       |    Ramp       |    SignalGenerator.RAMP        |
+                                   |   3       |    Triangular |    SignalGenerator.TRIANGULAR  |
+                                   |   4       |    TTL        |    SignalGenerator.TTL         |
+                                   ==============================================================
 
-    set_attenuation(float)           : The integer_level decides the attenuation level.
-                                       Though this can be configured to any custom floating point value, it is adviced to choose among the
-                                       standard attenuation levels using the SignalGenerator.ATTEN_LEVEL dict.
+    set_attenuation(float)        : The integer_level decides the attenuation level.
+                                    Though this can be configured to any custom floating
+                                    point value, it is adviced to choose among the
+                                    standard attenuation levels using the SignalGenerator.ATTEN_LEVEL dict.
 
-    set_amplitude(float_percent_max) : The amplitute is set as a linearly varying percent of the total maximum ( out of 100 ) for the current Attenuation level.
+    set_amplitude(float_percent_max) : The amplitute is set as a linearly varying percent of the total
+                                       maximum ( out of 100 ) for the current Attenuation level.
 
     set_outputs(output_Bus_2_bit)    : The output is linked to the 2 Bit Bus passed to the method.
                                        output_Bus_2_bit[0] will link to the signal generator output
                                        output_Bus_2_bit[1] will link to the SignalGenerator's GND
 
     set_frequency_range(tuple)       : set the frequency range of the SignalGenerator object.
-                                       NOTE: While this can be configured to any value, it is very strongly emphasized to use standard frequency ranges as
-                                       defined in the SignalGenerator.FREQ_RANGE dict which contains tuples of predefined ranges.
+                                       NOTE: While this can be configured to any value, it is very strongly
+                                       emphasized to use standard frequency ranges as defined in the
+                                       SignalGenerator.FREQ_RANGE dict which contains tuples of predefined ranges.
                                        This will ensure stable behaviour.
 
-    set_frequency(float_percent_max) : The final output frequency is the float_percent_max percent of the currently set frequency range.
+    set_frequency(float_percent_max) : The final output frequency is the float_percent_max percent of the
+                                       currently set frequency range.
 
-    set_frequency_exact(float)       : This method is used to set the exact output frequency of the SignalGenerator object.
-                                       If the given value is above SignalGenerator.FREQ_MAX or below SignalGenerator.FREQ_MIN the output frequency
-                                       saturates at the either extremes.
+    set_frequency_exact(float)       : This method is used to set the exact output frequency of the
+                                       SignalGenerator object. If the given value is above SignalGenerator.FREQ_MAX
+                                       or below SignalGenerator.FREQ_MIN the output frequency saturates at the either
+                                       extremes.
                                        NOTE: This method is preferred over setting custom frequency ranges.
 
-    set_amplitude_exact(float)       : This method is used to set the exact output amplitude of the SignalGenerator object.
-                                       If the given value is above SignalGenerator.AMPL_MAX or below SignalGenerator.AMPL_MIN the output amplitude
-                                       saturates at the either extremes.
+    set_amplitude_exact(float)       : This method is used to set the exact output amplitude of the SignalGenerator
+                                       object. If the given value is above SignalGenerator.AMPL_MAX or below
+                                       SignalGenerator.AMPL_MIN the output amplitude saturates at the either extremes.
                                        NOTE: This method is preferred over setting custom amplitude attenuation values.
 
     set_modulation_input(Bus_2_bit)  : Link the external modulation input to modulate the output accordingly.
@@ -128,57 +134,48 @@ class SignalGenerator(threading.Thread):
     USAGE
     =====
 
-    >>> sig1 = SignalGenerator(typ = SignalGenerator.SIN, freq = 1000, ampl = 5.0, modulation = SignalGenerator.NO_MOD)
+    >>> sig1 = SignalGenerator(typ = SignalGenerator.SIN, freq = 1000, ampl = 5.0)
     >>> a = Bus(2); a.set_type(analog=True);
     >>> sig1.set_output(a)
 
-    # Checking the output
+    Checking the output
+
     >>> start_time = time.time()
     >>> table = []
-    >>> while ( ( time.time() - start_time ) <= a.time_period ):
-            # Print values for 1 time period
-            table.append( len(table), ( float(a[0]) - float(a[1]) )) # Sampling table
-            time.sleep(a.time_period / float(10))
+    >>> while((time.time() - start_time) <= a.time_period):
+    ... # Print values for 1 time period
+    ... table.append(len(table), (float(a[0]) - float(a[1]))) # Sampling table
+    ... time.sleep(a.time_period/float(10))
 
-    >>> print ( table )
+    >>> print table
     (0, 0.2), ()
+
 
     AM SYNTHESIS USING 2 SIGNAL GENERATOR INSTANCES
     ===============================================
 
-    from BinPy import SignalGenerator
-    import numpy as np
-    import matplotlib.pyplot as plt
-    import math, time
-
-    m_t = SignalGenerator(typ = 0, freq = 10, ampl = 1)
-    m_t.set_offset(-0.5)
-
-    c_t = SignalGenerator(typ = 0, freq = 100, ampl = 10)
-    c_t.set_offset(-5) # To make the range as [-5, 5]
-
-    c_t.set_modulation_input(m_t.outputs)
-    c_t.set_modulation_type(1)
-
-    time.sleep(0.5) # To allow setup time
-
-    c_t.last_updated_time, (c_t.outputs[0].voltage - c_t.outputs[1].voltage)
-
-    data = np.zeros(shape = (2, math.ceil(m_t.time_period / c_t.sampling_time_interval)))
-
-    for i in range(data.shape[1]):
-        data[0][i] = m_t.last_updated_time + m_t.time_period * i
-        data[1][i] = c_t.outputs[0].voltage - c_t.outputs[1].voltage
-        time.sleep(c_t.sampling_time_interval)
-
-    fig, ax = plt.subplots()
-
-    ax.plot(data[0], data[1])
-
-    plt.show()
-
-    m_t.kill()
-    c_t.kill()
+    >>> from BinPy import SignalGenerator
+    >>> import numpy as np
+    >>> import matplotlib.pyplot as plt
+    >>> import math, time
+    >>> m_t = SignalGenerator(typ = 0, freq = 10, ampl = 1)
+    >>> m_t.set_offset(-0.5)
+    >>> c_t = SignalGenerator(typ = 0, freq = 100, ampl = 10)
+    >>> c_t.set_offset(-5) # To make the range as [-5, 5]
+    >>> c_t.set_modulation_input(m_t.outputs)
+    >>> c_t.set_modulation_type(1)
+    >>> time.sleep(0.5) # To allow setup time
+    >>> c_t.last_updated_time, (c_t.outputs[0].voltage - c_t.outputs[1].voltage)
+    >>> data = np.zeros(shape = (2, math.ceil(m_t.time_period / c_t.sampling_time_interval)))
+    >>> for i in range(data.shape[1]):
+    ... data[0][i] = m_t.last_updated_time + m_t.time_period * i
+    ... data[1][i] = c_t.outputs[0].voltage - c_t.outputs[1].voltage
+    ... time.sleep(c_t.sampling_time_interval)
+    >>> fig, ax = plt.subplots()
+    >>> ax.plot(data[0], data[1])
+    >>> plt.show()
+    >>> m_t.kill()
+    >>> c_t.kill()
 
     """
 
@@ -310,7 +307,10 @@ class SignalGenerator(threading.Thread):
         self._amplitude = float(ampl)
 
     def set_attenuation(self, attenuation):
-        """ Set the attenuation value. Use float values for input or use predefined dictionary SignalGenerator.ATTEN_LEVEL """
+        """
+        Set the attenuation value. Use float values for input or use predefined
+        dictionary SignalGenerator.ATTEN_LEVEL
+        """
         if (type(attenuation) not in [int, float]):
             raise Exception(
                 "ERROR: Attenuation can only be a float ( or int ) value.")
@@ -343,9 +343,11 @@ class SignalGenerator(threading.Thread):
 
     def set_frequency_range(self, range_tuple):
         """
-        Set the frequency range of the output. Use ranges within SignalGenerator.FREQ_MAX and SignalGenerator.FREQ_MIN.
-        Specify input as a tuple containg the LOW followed by the HIGH frequency limits. Preferably use the predefined constant dictionary
-        SignalGenerator.FREQ_RANGE
+        Set the frequency range of the output. Use ranges within
+        SignalGenerator.FREQ_MAX and SignalGenerator.FREQ_MIN.
+        Specify input as a tuple containg the LOW followed by the
+        HIGH frequency limits. Preferably use the predefined constant
+        dictionary SignalGenerator.FREQ_RANGE
 
         FREQ_RANGE    {
                          0 : ( 0.1, 10   ),
@@ -360,15 +362,15 @@ class SignalGenerator(threading.Thread):
         """
 
         if (type(range_tuple) not in [list, tuple]):
-            raise Exception(
+            raise TypeError(
                 "ERROR: Invalid input. Input can be specified only as a list or a tuple")
 
         if (len(range_tuple)) != 2:
-            raise Exception(
+            raise TypeError(
                 "ERROR: The length of the frequency range tuple is not 2")
 
         if (range_tuple[0] > range_tuple[1]):
-            raise Exception(
+            raise TypeError(
                 "ERROR: Low frequency limit is higher than the High frequency limit")
 
         if (range_tuple[0] < self.FREQ_MIN):
@@ -385,10 +387,10 @@ class SignalGenerator(threading.Thread):
         Set appropriate range before setting the frequency.
         """
         if (type(percent_range) not in [int, float]):
-            raise Exception("ERROR: Only int or float values can be passed.")
+            raise TypeError("ERROR: Only int or float values can be passed.")
 
         if (percent_range < 0 or percent_range > 100):
-            raise Exception(
+            raise ValueError(
                 "ERROR: Only values between 1 to 100 can be passed")
 
         self._frequency = float(self._frequency_range[
@@ -406,7 +408,7 @@ class SignalGenerator(threading.Thread):
         Set the given frequency as the freqency of the output. The current range will be varied based on the value.
         """
         if (type(frequency) not in [int, float]):
-            raise Exception(
+            raise TypeError(
                 "ERROR: Invalid Input type. Frequency can only be a float value")
 
         if (frequency < self.FREQ_MIN):
@@ -453,27 +455,31 @@ class SignalGenerator(threading.Thread):
             self._type = typ
 
         else:
-            raise Exception(
-                "ERROR: Invalid input. Please give a numerical value between 0 and 4 ( both inclusive ) ")
+            raise ValueError(
+                "ERROR: Invalid input. Please give a numerical value "
+                "between 0 and 4 ( both inclusive ) ")
 
     def set_offset(self, offset):
         """
         Set a DC offset voltage for the output waveform
-        Range of the acceptable offset value is [ -SignalGenerator.AMPL_MAX, SignalGenerator.AMPL_MIN ]
+        Range of the acceptable offset value is
+        [ -SignalGenerator.AMPL_MAX, SignalGenerator.AMPL_MIN ]
         """
         if type(offset) not in [float, int]:
-            raise Exception(
-                "ERROR: Offset can be a float ( or int value ) withing the specified range only.")
+            raise TypeError(
+                "ERROR: Offset can be a float (or int value) within "
+                "the specified range only.")
 
         if (offset > self.AMPL_MAX) or (offset < -self.AMPL_MAX):
-            raise Exception(
+            raise TypeError(
                 "ERROR: Invalid offset value. Specify offset within the range")
 
         self._offset = float(offset)
 
     def set_enable(self, enable):
         """
-        Link the enable connector ( or Bus_1_bit ) with the SignalGenerator object's enable input.
+        Link the enable connector (or Bus_1_bit) with the SignalGenerator
+        object's enable input.
         """
 
         with AutoUpdater._lock:
@@ -483,18 +489,19 @@ class SignalGenerator(threading.Thread):
                     enable,
                     self._enable)
             else:
-                raise Exception(
-                    "ERROR: Invalid Enable input. Enable must be a 1-bit Bus or a Connector.")
+                raise ValueError(
+                    "ERROR: Invalid Enable input. Enable must be a "
+                    "1-bit Bus or a Connector.")
 
     def set_outputs(self, outputs):
         """
         Set the outputs to be linked with the outputs of the SignalGenerator
         """
         if not isinstance(outputs, Bus):
-            raise Exception("ERROR: Invalid output Bus")
+            raise TypeError("ERROR: Invalid output Bus")
 
         if (outputs.width != self.outputs.width):
-            raise Exception("ERROR: Output width mismatch.")
+            raise TypeError("ERROR: Output width mismatch.")
 
         with AutoUpdater._lock:
             AutoUpdater.remove_link(self.outputs)
@@ -508,7 +515,7 @@ class SignalGenerator(threading.Thread):
         which will act as a carrier signal
         """
         if mod_type not in [0, 1, 2]:
-            raise Exception(
+            raise ValueError(
                 "ERROR: Invalid input for modulation type. Allowed values are 0, 1 or 2")
 
         self._mod_type = mod_type
@@ -518,7 +525,7 @@ class SignalGenerator(threading.Thread):
         Link the mod_input Analog Bus of width 2, with the modulating input.
         """
         if (not isinstance(mod_input, Bus)) or (not mod_input.analog):
-            raise Exception(
+            raise TypeError(
                 "ERROR: Invalid modulation input. The modulation input must be 2 connector analog Bus.")
 
         if (mod_input.width != self.mod_ip.width):
