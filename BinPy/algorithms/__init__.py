a120130a66864454707f6d3816cc1e399b0296c5
Raghav R V <rvraghav93@gmail.com>
2014-07-28 04:47:44 +0530

    Added MultiplicationAlgos module in __init__

--- BinPy/algorithms/__init__.py
+++ BinPy/algorithms/__init__.py
@@ -4,3 +4,4 @@ from BinPy.algorithms.MooreOptimizer import *
 from BinPy.algorithms.AnalogFormulas import *
 from BinPy.algorithms.ExpressionConvert import *
 from BinPy.algorithms.bittools import *
+from BinPy.algorithms.MultiplicationAlgos import *
