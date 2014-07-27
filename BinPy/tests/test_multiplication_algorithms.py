6d055e06dc56f8ed1c99542b0d3cd2a98bd9b409
Raghav R V <rvraghav93@gmail.com>
2014-07-28 05:07:32 +0530

    Added test for booths and karatsuba's algorithms

---
+++ BinPy/tests/test_multiplication_algorithms.py
@@ -0,0 +1,111 @@
+from BinPy import *
+
+
+def test_karatsuba():
+
+    assert BitTools.to_signed_int(
+        Multipliers.karatsuba_multiply(bin(-7), bin(5), signed=True)) == -35
+
+    assert BitTools.to_signed_int(
+        Multipliers.karatsuba_multiply(bin(-7), bin(-5), signed=True)) == 35
+
+    assert BitTools.to_signed_int(
+        Multipliers.karatsuba_multiply(
+            '111111',
+            '111111',
+            7)) == 3969
+
+    assert BitTools.to_signed_int(
+        Multipliers.karatsuba_multiply(
+            '111111',
+            '111111',
+            5,
+            signed=True)) == 1
+
+    assert BitTools.to_signed_int(
+        Multipliers.karatsuba_multiply(
+            '1111',
+            '0111',
+            5,
+            signed=False)) == 105
+
+    assert BitTools.to_signed_int(
+        Multipliers.karatsuba_multiply(
+            '1111',
+            '0111',
+            5,
+            signed=True)) == -7
+
+    assert BitTools.to_signed_int(
+        Multipliers.karatsuba_multiply(
+            '-1',
+            '0111',
+            5,
+            signed=True)) == -7
+
+    try:
+        # If no of bits explicitly specified
+        BitTools.to_signed_int(
+            Multipliers.karatsuba_multiply(
+                '111111',
+                '111111',
+                5,
+                signed=True))
+        assert False
+    except:
+        pass
+
+
+def test_booths():
+
+    assert BitTools.to_signed_int(
+        Multipliers.booths_multiply(bin(-7), bin(5), 4)) == -35
+
+    assert BitTools.to_signed_int(
+        Multipliers.booths_multiply(bin(-7), bin(-5), 4)) == 35
+
+    assert BitTools.to_signed_int(
+        Multipliers.booths_multiply(
+            '111111',
+            '111111',
+            7)) == 3969
+
+    assert BitTools.to_signed_int(
+        Multipliers.booths_multiply(
+            '111111',
+            '111111',
+            5,
+            signed=True)) == 1
+
+    assert BitTools.to_signed_int(
+        Multipliers.booths_multiply(
+            '1111',
+            '0111',
+            5,
+            signed=False)) == 105
+
+    assert BitTools.to_signed_int(
+        Multipliers.booths_multiply(
+            '1111',
+            '0111',
+            5,
+            signed=True)) == -7
+
+    assert BitTools.to_signed_int(
+        Multipliers.booths_multiply(
+            '-1',
+            '0111',
+            5,
+            signed=True)) == -7
+
+    try:
+        # If no of bits explicitly specified
+        BitTools.to_signed_int(
+            Multipliers.booths_multiply(
+                '111111',
+                '111111',
+                5,
+                signed=True))
+        assert False
+    except:
+        pass
