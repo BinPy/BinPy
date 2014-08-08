0a5303da4474cc79897303f16a003b032301b7d6
Raghav R V <rvraghav93@gmail.com>
2014-08-08 14:12:57 +0530

    Added toom3 multiplication algorithm

--- BinPy/algorithms/MultiplicationAlgos.py
+++ BinPy/algorithms/MultiplicationAlgos.py
@@ -37,7 +37,7 @@ def booths_multiply(multiplicand, multiplier, bits=None, signed=False):
 
     if bits is None:
 
-        # DEV NOTE: Do not replace 0b in the inputs, the BitTools will
+        # DEV NOTE: Do not replace 0b in the inputs, the BinPyBits will
         # handle the same.
 
         if not signed:
@@ -114,7 +114,7 @@ def robertsons_multiply(multiplicand, multiplier, bits=None, signed=False):
 
     if bits is None:
 
-        # DEV NOTE: Do not replace 0b in the inputs, the BitTools will
+        # DEV NOTE: Do not replace 0b in the inputs, the BinPyBits will
         # handle the same.
 
         if not signed:
@@ -208,7 +208,7 @@ def karatsuba_multiply(multiplier, multiplicand, bits=None, signed=False):
 
     if bits is None:
 
-        # DEV NOTE: Do not replace 0b in the inputs, the BitTools will
+        # DEV NOTE: Do not replace 0b in the inputs, the BinPyBits will
         # handle the same.
 
         if not signed:
@@ -303,3 +303,170 @@ def karatsuba_multiply(multiplier, multiplicand, bits=None, signed=False):
         abs_result.int *= -1
 
     return abs_result.bin
+
+
+def toom3_multiply(multiplier, multiplicand, bits=None, signed=False):
+    """
+    Multiply the multiplicand ( binary represented ) and multiplier ( binary represented )
+    based on Toom Cook's multiplication algorithm - ( Toom-3; Km = Kn = 3 ).
+    If the signed is True, the input_data is assumed to be in a signed binary representation.
+
+    The result is a signed binary string representing the product.
+
+    USAGE:
+
+    # Both Positive inputs
+    >>>  toom3_multiply('0101', '0111')
+    '0000100011'
+
+    # Passing 2s Complement Signed binary string as one input
+    >>> product =  toom3_multiply('0101','1001')
+    >>> print ( product )
+    '1111011101'
+    >>> to_signed_int(product)
+    -35
+
+    # Passing - signed binary string
+    >>> product =  toom3_multiply('101','-111')
+    >>> print ( product )
+    '1111011101'
+    >>> to_signed_int(product)
+    -35
+
+    """
+
+    print multiplicand, multiplier
+
+    if bits is None:
+
+        # DEV NOTE: Do not replace 0b in the inputs, the BinPyBits will
+        # handle the same.
+
+        if not signed:
+            if "0b" not in multiplier:
+                multiplier = multiplier.lstrip("0")
+
+            if "0b" not in multiplicand:
+                multiplicand = multiplicand.lstrip("0")
+
+        bits = max(len(multiplier), len(multiplicand))
+
+        if bits == len(multiplier) and "0b" in multiplier:
+            bits -= 2
+
+        elif bits == len(multiplicand) and "0b" in multiplicand:
+            bits -= 2
+
+        if bits == 0:
+            bits = 1
+
+    len_input = bits
+
+    while bits % 3 != 0:
+        bits += 1
+
+    multiplicand = BinPyBits(multiplicand, bits, signed)
+    multiplier = BinPyBits(multiplier, bits, signed)
+
+    sign_bit = None
+
+    if (signed):
+        # The following set of operations will run only during the first
+        # pass, if the signed is set to True
+
+        # Calculating the sign of the product
+        if ((multiplicand.bin[0] == "1") ^ (multiplier.bin[0] == "1")):
+            sign_bit = 1
+        else:
+            sign_bit = 0
+
+        # Strip off the sign bit and convert to a BitArray with unsigned
+        # binary string.
+        multiplicand = BinPyBits(
+            abs(multiplicand.int), bits, signed=False)
+        multiplier = BinPyBits(
+            abs(multiplier.int), bits, signed=False)
+
+    # Base case of 0 bit multiplication. If length is 0 product is 0.
+    if len(multiplier.bin.lstrip("0")) == 0 or len(multiplicand.bin.lstrip("0")) == 0:
+        return "0"
+
+    # Base case of 1 bit multiplication
+    if len(multiplier.bin.lstrip("0")) == 1:
+        return "1" if (
+            multiplier.bin[-1] == "1" and multiplicand.bin[-1] == "1") else "0"
+
+    m = int(bits / 3)
+
+    B = 2 ** m
+
+    # x = x2*(B**2) + x1*(B) + x0 <= p(x = B)
+    # y = y2*(B**2) + y1*(B) + y0 <= q(x = B)
+
+    x2 = BinPyBits(multiplicand.bin[:m])
+    x1 = BinPyBits(multiplicand.bin[m:2 * m])
+    x0 = BinPyBits(multiplicand.bin[2 * m:])
+
+    y2 = BinPyBits(multiplier.bin[:m])
+    y1 = BinPyBits(multiplier.bin[m:2 * m])
+    y0 = BinPyBits(multiplier.bin[2 * m:])
+
+    # Multipoint coefficient evaluation using Marco Bodrato's sequence
+
+    # p is the multipoint dict of cartesian pairs for the multiplicand polynomial
+    # q is the multipoint dict of cartesian pairs for the multiplier polynomial
+
+    p = {}
+    q = {}
+
+    p_o = BinPyBits(x0.uint + x2.uint)
+
+    p[0] = x0
+    p[1] = BinPyBits(p_o.uint + x1.uint)
+    p[-1] = BinPyBits(p_o.uint - x1.uint)  # Can be negative
+    p[-2] = BinPyBits(2 * (p[-1].uint + x2.uint) - x0.uint)  # Can be negative
+    p['inf'] = x2
+
+    q_o = BinPyBits(x0.uint + x2.uint)
+
+    q[0] = x0
+    q[1] = BinPyBits(q_o.uint + x1.uint)
+    q[-1] = BinPyBits(q_o.uint - x1.uint)  # Can be negative
+    q[-2] = BinPyBits(2 * (q[-1].uint + x2.uint) - x0.uint)  # Can be negative
+    q['inf'] = x2
+
+    # Calculating pointwise multiplication - Recursively calling toom3 for
+    # smaller part multiplications
+
+    r = {}
+
+    # print p, q
+
+    r[0] = BinPyBits(toom3_multiply(p[0].bin, q[0].bin))
+    r[1] = BinPyBits(toom3_multiply(p[1].bin, q[1].bin))
+    r[-1] = BinPyBits(toom3_multiply(p[-1].bin, q[-1].bin))
+    r[-2] = BinPyBits(toom3_multiply(p[-2].bin, q[-2].bin))
+    r['inf'] = BinPyBits(toom3_multiply(p['inf'].bin, q['inf'].bin))
+
+    # Interpolating the points to the product polynomial
+    # Using Marco Bodrato's sequence
+    # Integer ( may be negative too ) result of the point multiplication is
+    # used to interpolate
+
+    r0 = r[0].int
+    r4 = r['inf'].int
+    r3 = (r[-2].int - r[1].int) / 3
+    r1 = (r[1].int - r[-1].int) / 2
+    r2 = r[-1].int - r[0].int
+    r3 = (r2 - r3) / 2 + 2 * r['inf']
+    r2 = r2 + r1 - r4
+    r1 = r1 - r3
+
+    product = r4 * (4 * B) + r3 * (3 * B) + r2 * (2 * B) + r1 * (B) + r0
+
+    product = BinPyBits(product, 2 * len_input)
+
+    if sign_bit == 1:
+        product.int *= -1
+
+    return product.bin
