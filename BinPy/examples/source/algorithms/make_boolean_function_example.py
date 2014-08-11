a61148a71a022b6877af0445f799f3dfa3eb6136
Raghav R V <rvraghav93@gmail.com>
2014-08-12 00:54:32 +0530

    Update python sources for algorithms

--- BinPy/examples/source/algorithms/make_boolean_function_example.py
+++ BinPy/examples/source/algorithms/make_boolean_function_example.py
@@ -1,45 +1,41 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
 
-# coding: utf-8
+# <headingcell level=2>
 
-### An example to demostrate the usage of make boolean function.
+# An example to demostrate the usage of make boolean function.
 
-# In[1]:
+# <codecell>
 
 from __future__ import print_function
 from BinPy.algorithms.makebooleanfunction import *
 
-
-# In[2]:
+# <codecell>
 
 # Usage of make_boolean() function
 logical_expression, gate_form = make_boolean(['A', 'B', 'C'], [1, 4, 7], minterms=True)
 
-
-# In[3]:
+# <codecell>
 
 # Print the logical function
 print(logical_expression)
 
-
-# In[4]:
+# <codecell>
 
 # Print the gate form
 print(gate_form)
 
-
-# In[5]:
+# <codecell>
 
 # Another example
 logical_expression, gate_form = make_boolean(['A', 'B', 'C', 'D'], [1, 4, 7, 0], maxterms=True)
 
-
-# In[6]:
+# <codecell>
 
 # Print the logical function
 print(logical_expression)
 
-
-# In[7]:
+# <codecell>
 
 # Print the gate form
 print(gate_form)
