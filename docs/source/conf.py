ccd9de5048109428ce9597f0d33acfb9dd927fc0
Himanshu Mishra <OrkoHunter@users.noreply.github.com>
2015-07-01 20:15:20 +0530

    Add napoleon extension for numpydoc doc style views

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,6 +36,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.pngmath',
     'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
